fae98b0764ff04db4b278bb459d763699fc90547
jbeficent <jordi.ballester@eficent.com>
2015-02-02 14:37:30 +0100

    Fixes to module "stock_analytic_account"

--- stock_analytic_account/__openerp__.py
+++ stock_analytic_account/__openerp__.py
@@ -28,14 +28,20 @@
     'summary': 'Adds the analytic account to stock moves',
     "depends": ["stock", "analytic"],
     "description": """
+Project Procurement
+===================
+Features of this module:
     - Adds the analytic account to the stock move
     - Makes it possible to search stock moves by analytic account or its project manager
     - Makes it possible to search picking lists by analytic account or its project manager
+    - Adds button in the Project Form and an Action from Project's 'More' menu to list the
+    Procurement Orders associated to the selected project.
     """,
     "init_xml": [],
     'data': [
-             'stock_view.xml',
-             'stock_picking_view.xml',
+             'view/stock_view.xml',
+             'view/stock_picking_view.xml',
+             'view/analytic_account_view.xml',
     ],
     'test':[
     ],
