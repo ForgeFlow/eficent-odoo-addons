9c75d2b6c5d3284d903c5195e5c844aa5048ba51
jbeficent <jordi.ballester@eficent.com>
2015-02-04 12:19:05 +0100

    [ADD] Module 'purchase_cancel_services'.

--- analytic_resource_plan_purchase/wizard/analytic_resource_plan_line_make_purchase.py
+++ analytic_resource_plan_purchase/wizard/analytic_resource_plan_line_make_purchase.py
@@ -94,113 +94,112 @@ class analytic_resource_plan_line_make_purchase(orm.TransientModel):
 
             for line in line_plan_obj.browse(cr, uid, record_ids,
                                              context=context):
+                if not line.supplier_id:
+                    raise osv.except_osv(
+                        _('Could not create purchase order !'),
+                        _('You have to enter a supplier.'))
 
-                    if not line.supplier_id:
-                        raise osv.except_osv(
-                            _('Could not create purchase order !'),
-                            _('You have to enter a supplier.'))   
-                    
-                    if supplier_data is not False \
-                            and line.supplier_id.id != supplier_data:
-                        raise osv.except_osv(
-                            _('Could not create purchase order !'),
-                            _('You have to select lines '
-                              'from the same supplier.'))
-                    else:
-                        supplier_data = line.supplier_id.id
+                if supplier_data is not False \
+                        and line.supplier_id.id != supplier_data:
+                    raise osv.except_osv(
+                        _('Could not create purchase order !'),
+                        _('You have to select lines '
+                          'from the same supplier.'))
+                else:
+                    supplier_data = line.supplier_id.id
 
-                    address_id = partner_obj.address_get(
-                        cr, uid, [line.supplier_id.id],
-                        ['delivery'])['delivery']
-                    partner = line.supplier_id
-                    line_company_id = line.company_id \
-                        and line.company_id.id or False
-                    if company_id is not False \
-                            and line_company_id != company_id:
-                        raise osv.except_osv(
-                            _('Could not create purchase order !'),
-                            _('You have to select lines '
-                              'from the same company.'))
-                    else:
-                        company_id = line_company_id        
-                    
-                    line_account_id = line.account_id \
-                        and line.account_id.id or False
+                address_id = partner_obj.address_get(
+                    cr, uid, [line.supplier_id.id],
+                    ['delivery'])['delivery']
+                partner = line.supplier_id
+                line_company_id = line.company_id \
+                    and line.company_id.id or False
+                if company_id is not False \
+                        and line_company_id != company_id:
+                    raise osv.except_osv(
+                        _('Could not create purchase order !'),
+                        _('You have to select lines '
+                          'from the same company.'))
+                else:
+                    company_id = line_company_id
 
-                    account_id = line_account_id
-                    
-                    warehouse_obj = self.pool.get('stock.warehouse')
-                    warehouse_ids = warehouse_obj.search(
-                        cr, uid, [('company_id', '=', company_id)])
-                    if warehouse_ids:
-                        warehouses = warehouse_obj.browse(
-                            cr, uid, warehouse_ids, context=context)
-                        location_ids = []
-                        for lot_stock_ids in warehouses:
-                            location_ids.append(lot_stock_ids.lot_stock_id.id)
+                line_account_id = line.account_id \
+                    and line.account_id.id or False
 
-                    location_id = False
-                    if location_ids:
-                        location_id = location_ids[0]
+                account_id = line_account_id
 
-                    purchase_order_line = {
-                        'name': line.name,
-                        'product_qty': line.unit_amount,
-                        'product_id': line.product_id.id,
-                        'product_uom': line.product_uom_id.id,
-                        'price_unit': line.price_unit,
-                        'date_planned': line.date,
-                        'notes': line.notes,
-                        'account_analytic_id': account_id,
-                    }
-                    taxes = False
-                    if line.product_id:
-                        taxes_ids = \
-                            line.product_id.product_tmpl_id.supplier_taxes_id
-                        taxes = acc_pos_obj.map_tax(
-                            cr, uid, partner.property_account_position, taxes_ids)
-                    
-                    if taxes:
-                        purchase_order_line.update({
-                            'taxes_id': [(6, 0, taxes)]
-                        })
-                    list_line.append(purchase_order_line)
-                    
-                    if purchase_id is False:
-                        purchase_id = order_obj.create(
-                            cr, uid,
-                            {'origin': '',
-                             'partner_id': line.supplier_id.id,
-                             'partner_address_id': address_id,
-                             'pricelist_id': line.pricelist_id.id,
-                             'location_id': location_id,
-                             'company_id': company_id,
-                             'fiscal_position':
-                                partner.property_account_position
-                                and partner.property_account_position.id
-                                or False,
-                             'payment_term':
-                                partner.property_supplier_payment_term
-                                and partner.property_supplier_payment_term.id
-                                or False
-                             }, context=context)
-                        if line.account_id.user_id:
-                            order_obj.message_subscribe_users(
-                                cr, uid, [purchase_id],
-                                user_ids=[line.account_id.user_id.id])
-                                                        
+                warehouse_obj = self.pool.get('stock.warehouse')
+                warehouse_ids = warehouse_obj.search(
+                    cr, uid, [('company_id', '=', company_id)])
+                if warehouse_ids:
+                    warehouses = warehouse_obj.browse(
+                        cr, uid, warehouse_ids, context=context)
+                    location_ids = []
+                    for lot_stock_ids in warehouses:
+                        location_ids.append(lot_stock_ids.lot_stock_id.id)
+
+                location_id = False
+                if location_ids:
+                    location_id = location_ids[0]
+
+                purchase_order_line = {
+                    'name': line.name,
+                    'product_qty': line.unit_amount,
+                    'product_id': line.product_id.id,
+                    'product_uom': line.product_uom_id.id,
+                    'price_unit': line.price_unit,
+                    'date_planned': line.date,
+                    'notes': line.notes,
+                    'account_analytic_id': account_id,
+                }
+                taxes = False
+                if line.product_id:
+                    taxes_ids = \
+                        line.product_id.product_tmpl_id.supplier_taxes_id
+                    taxes = acc_pos_obj.map_tax(
+                        cr, uid, partner.property_account_position, taxes_ids)
+
+                if taxes:
                     purchase_order_line.update({
-                        'order_id': purchase_id
+                        'taxes_id': [(6, 0, taxes)]
                     })
-                    
-                    order_line_id = order_line_obj.create(
-                        cr, uid, purchase_order_line, context=context)
-                    values = {
-                        'order_line_ids': [(4, order_line_id)]
-                    }
-                    line_plan_obj.write(cr, uid, [line.id], values,
-                                        context=context)
-                    res.append(order_line_id)
+                list_line.append(purchase_order_line)
+
+                if purchase_id is False:
+                    purchase_id = order_obj.create(
+                        cr, uid,
+                        {'origin': '',
+                         'partner_id': line.supplier_id.id,
+                         'partner_address_id': address_id,
+                         'pricelist_id': line.pricelist_id.id,
+                         'location_id': location_id,
+                         'company_id': company_id,
+                         'fiscal_position':
+                            partner.property_account_position
+                            and partner.property_account_position.id
+                            or False,
+                         'payment_term':
+                            partner.property_supplier_payment_term
+                            and partner.property_supplier_payment_term.id
+                            or False
+                         }, context=context)
+                    if line.account_id.user_id:
+                        order_obj.message_subscribe_users(
+                            cr, uid, [purchase_id],
+                            user_ids=[line.account_id.user_id.id])
+
+                purchase_order_line.update({
+                    'order_id': purchase_id
+                })
+
+                order_line_id = order_line_obj.create(
+                    cr, uid, purchase_order_line, context=context)
+                values = {
+                    'order_line_ids': [(4, order_line_id)]
+                }
+                line_plan_obj.write(cr, uid, [line.id], values,
+                                    context=context)
+                res.append(order_line_id)
 
         return {
             'domain': "[('id','in', ["+','.join(map(str, res))+"])]",
