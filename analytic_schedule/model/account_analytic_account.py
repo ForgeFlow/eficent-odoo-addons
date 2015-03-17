67fdd62106fdc9a6e0e0c42f757c7d6b2db4e609
jbeficent <jordi.ballester@eficent.com>
2015-03-17 12:51:23 +0100

    Changes to introduce the automatic scheduling on analytic accounts, and the percent completed calculation, based on the duration of the project and the progress measurement entered.

---
+++ analytic_schedule/model/account_analytic_account.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2015 Eficent (<http://www.eficent.com/>)
+#               <contact@eficent.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.osv import fields, orm
+
+
+class account_analytic_account(orm.Model):
+    _inherit = "account.analytic.account"
+
+    def _compute_scheduled_dates(self, cr, uid, analytic, context=None):
+        #Obtain the earliest and latest dates of the children
+        start_dates = []
+        end_dates = []
+        for child in analytic.child_ids:
+            start_dates.append(child.date_start)
+            end_dates.append(child.date)
+        vals = {
+            'date_start': min(start_dates) or False,
+            'date': max(end_dates) or False,
+        }
+        self.write(cr, uid, analytic.id, vals, context=context)
+        return True
+
+    def write(self, cr, uid, ids, vals, context=None):
+        res = super(account_analytic_account, self).write(cr, uid, ids, vals,
+                                                          context=context)
+        if 'date_start' in vals or 'date' in vals:
+            for acc in self.browse(cr, uid, ids, context=context):
+                if not acc.parent_id:
+                    return res
+                self._compute_scheduled_dates(cr, uid, acc.parent_id,
+                                              context=context)
