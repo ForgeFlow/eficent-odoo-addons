2ec570d6684bc477ec92b4b9d10233da3ecaca4c
jbeficent <jordi.ballester@eficent.com>
2015-03-17 12:51:23 +0100

    Changes to introduce the automatic scheduling on analytic accounts, and the percent completed calculation, based on the duration of the project and the progress measurement entered.

---
+++ analytic_schedule/__openerp__.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2015 Eficent (<http://www.eficent.com/>)
+#             <contact@eficent.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+{
+    "name": "Analytic Schedule",
+    "version": "1.0",
+    "author": "Eficent",
+    "website": "www.eficent.com",
+    "category": "Generic Modules/Projects & Services",
+    "depends": ["analytic"],
+    "description": """
+Analytic Schedule
+=================
+The project / analytic account's start and end dates are automatically
+computed based on the earliest start and latest finish date of the
+children.
+    """,
+    "init_xml": [],
+    "update_xml": [],
+    'demo_xml': [
+
+    ],
+    'test':[
+    ],
+    'installable': True,
+    'active': False,
+    'certificate': '',
+    'application': True,
+}
