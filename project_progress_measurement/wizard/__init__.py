9db9a3823f004c49071c336caa3bcbe20266e7e8
jbeficent <jordi.ballester@eficent.com>
2015-09-30 12:21:56 +0200

    [IMP] add wizard for quick project progress measurement entry

--- project_progress_measurement/wizard/__init__.py
+++ project_progress_measurement/wizard/__init__.py
@@ -18,5 +18,5 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-
-import progress_measurements_entry
+from . import progress_measurements_entry
+from . import progress_measurements_quick_entry
