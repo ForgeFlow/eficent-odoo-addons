284d171aba81fc75120e9e6ddd28ea07cd1ad89e
jbeficent <jordi.ballester@eficent.com>
2016-02-08 16:18:46 +0100

    [ADD] plm_component_no_create

---
+++ plm_component_no_create/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © 2016 Eficent Business and IT Consulting Services S.L.
+# - Jordi Ballester Alomar
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": 'PLM Component no create',
+    "summary": "Disables product creation in product view of OdooPLM",
+    "version": "9.0.1.0.0",
+    "author": "Eficent Business and IT Consulting Services S.L.",
+    "website": "http://www.eficent.com",
+    "category": "Manufacturing",
+    "depends": ['plm'],
+    "license": "LGPL-3",
+    "data": [
+        "views/product_view.xml",
+    ],
+    "installable": True,
+}
