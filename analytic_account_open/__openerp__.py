6e58f6b525258a888b9da891d34a4ebd13ed25d5
jbeficent <jordi.ballester@eficent.com>
2016-02-22 13:08:08 +0100

    [ADD] analytic_account_open

---
+++ analytic_account_open/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# © 2015 Eficent - Jordi Ballester Alomar
+# © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Project WBS Open",
+    "version": "7.0.1.0.0",
+    "summary": "Opens a single project/analytic account, or the whole WBS",
+    "author": "Eficent",
+    "website": "http://www.eficent.com",
+    "category": "Generic",
+    "depends": ["account", "analytic"],
+    "license": "AGPL-3",
+    "data": [
+        "wizards/analytic_account_open_view.xml",
+    ],
+    'installable': True,
+    'active': False,
+}
