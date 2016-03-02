b1734d71adea4876f3fb1d67f8efe5e754290d01
jbeficent <jordi.ballester@eficent.com>
2016-03-02 14:02:07 +0100

    [IMP] search by customer

--- change_management_customer_ref/models/change_management.py
+++ change_management_customer_ref/models/change_management.py
@@ -16,16 +16,32 @@ class ChangeManagementChange(orm.Model):
                 result.add(change.id)
         return list(result)
 
+    def _get_project_customer(self, cr, uid, ids, field_name, arg,
+                              context=None):
+        result = {}
+        for change in self.browse(cr, uid, ids, context=context):
+            result[change.id] = change.project_id.partner_id.id
+        return result
+
+    def _search_by_project_customer(self, cr, uid, obj, name, args, context):
+        change_ids = {}
+        for cond in args:
+            partner_id = cond[2]
+            project_ids = self.pool['project.project'].search(
+                cr, uid, [('partner_id', '=', partner_id)])
+            change_ids = self.pool['change.management.change'].search(
+                cr, uid, [('project_id', 'in', project_ids)])
+        if change_ids:
+            return [('id', 'in', tuple(change_ids))]
+        else:
+            return False
+
     _columns = {
 
-        'customer_id':  fields.related('project_id', 'partner_id',
-                                       type='many2one', relation='res.partner',
-                                       string='Customer',
-                                       readonly=True,
-                                       store={
-                                           'project.project': (
-                                               _get_change_orders_project,
-                                               ['partner_id'], 20)}),
+        'customer_id':  fields.function(
+            _get_project_customer, method=True, type='many2one',
+            relation='res.partner', string='Customer', readonly=True,
+            store=False, fnct_search=_search_by_project_customer),
 
         'customer_ref': fields.char('Customer ref.',
                                     help="Reference of the Change as "
