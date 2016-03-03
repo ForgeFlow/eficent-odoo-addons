0d4717a9c69520c8ec55dcb895afa15634f86f65
jbeficent <jordi.ballester@eficent.com>
2016-03-03 14:24:51 +0100

    [ADD] analytic_wip_report_contract_value

---
+++ analytic_wip_report_contract_value/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# © 2016 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Work In Progress Statement - Contract Value",
+    "version": "7.0.1.0.0",
+    "author": "Eficent Business and IT Consulting Services S.L.",
+    "website": "",
+    "category": "Generic Modules/Projects & Services",
+    "depends": [
+        "analytic_wip_report",
+        "analytic_contract_value"
+    ],
+    "data": [
+        "views/account_analytic_account_view.xml",
+    ],
+    'installable': True,
+    'active': False,
+}
