09e95e28504a5e74ebee0cfb3003dd3029f64c17
jbeficent <jordi.ballester@eficent.com>
2016-06-21 11:50:02 +0200

    [FIX] fix sequence modules

---
+++ stock_picking_move_seq/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# © 2015 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Stock Picking Move Sequence",
+    "version": "7.0.1.0.0",
+    "author": "Eficent Business and IT Consulting Services S.L.",
+    "category": "Purchases",
+    "website": "http://www.eficent.com/",
+    "license": "AGPL-3", 
+    "depends": [
+        "stock_picking_reorder_lines"
+    ], 
+    "demo": [], 
+    "data": [
+        "view/stock_picking_view.xml"
+    ],
+    "installable": True, 
+    "auto_install": False, 
+    "active": False
+}
