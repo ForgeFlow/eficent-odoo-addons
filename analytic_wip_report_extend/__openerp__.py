51685e106033947e12575c002313cbb64a8cd54e
ahenriquez <ahenriquez@eficent.com>
2016-09-22 14:39:42 +0200

    [ADD] analytic wip report extend

---
+++ analytic_wip_report_extend/__openerp__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# © 2016 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+
+{
+    "name": "Work In Progress Statement for PRI",
+    "version": "7.0.1.0.0",
+    "author": "Eficent Business and IT Consulting Services S.L.",
+    "category": "Customizations for PRI",
+    "description": """
+Purchase for PRI
+======================================
+Contains customizations for PRI on the work in progress statement module
+
+""",
+    "website": "http://www.eficent.com/",
+    "license": "",
+    "depends": [
+        "analytic_wip_report",
+    ],
+    "data": [
+        "account_analytic_account_view.xml"
+    ],
+    "installable": True,
+    "auto_install": False,
+    "active": False
+}
---
+++ analytic_wip_report_extend/account_analytic_account.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+# © 2016 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+import decimal_precision as dp
+from openerp.osv import fields, osv
+
+
+class account_analytic_account(osv.osv):
+    
+    _inherit = 'account.analytic.account'
+
+    def _wip_report(self, cr, uid, ids, fields, arg, context=None):
+        res = super(account_analytic_account, self)._wip_report(cr, uid,
+                                                                ids, fields,
+                                                                arg,
+                                                                context)
+        if context is None:
+            context = {}
+
+        for account in self.browse(cr, uid, ids, context=context):
+            # Estimated gross profit percentage
+            try:
+                res[account.id]['estimated_gross_profit_per'] = \
+                    res[account.id]['estimated_gross_profit'] / \
+                    res[account.id]['total_value'] * 100
+            except ZeroDivisionError:
+                res[account.id]['estimated_gross_profit_per'] = 0
+            # Over/Under billings
+            over_under_billings = res[account.id]['under_billings'] - \
+                                  res[account.id]['over_billings']
+            res[account.id]['under_over'] = over_under_billings
+        return res
+
+    _columns = {
+        'estimated_gross_profit_per': fields.function(
+            _wip_report, method=True, type='float', string='Total Value',
+            multi='wip_report',
+            help="Estimated gros profit percentage (estimated gross profit/"
+                 "total contract value)",
+            digits_compute=dp.get_precision('Account')),
+
+        'under_over': fields.function(
+            _wip_report, method=True, type='float', multi='wip_report',
+            help="Total under/over (under_billed-over_billed)"),
+        }
