fe37d2880ee4e7327fef94e0603bc991a401595d
ahenriquez <ahenriquez@eficent.com>
2016-09-23 18:27:19 +0200

    [ADD] project add end date when closing

---
+++ project_closing_date/__openerp__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# © 2016 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+
+{
+    "name": "Project closing date",
+    "version": "1.0",
+    "author": "Eficent",
+    "website": "",
+    "category": "Generic Modules/Projects & Services",
+    "depends": [
+                "project",
+                ],
+    "description": """
+Project closed
+====================================
+Fills the date end when project is closed.
+
+More information and assistance:
+-----------------------------------
+    If you are interested in this module and seek further assistance to use it please visit
+    us at www.eficent.com or conact us at contact@eficent.com.
+
+    """,
+
+    "data": [
+    ],
+    'test':[
+    ],
+    'installable': True,
+    'active': False,
+    'certificate': '',
+}
