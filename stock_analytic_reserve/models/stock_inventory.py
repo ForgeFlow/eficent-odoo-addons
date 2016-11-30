8f72f328ecc597f4a5250f0de880cc41c945c9e9
ahenriquez <ahenriquez@eficent.com>
2016-11-30 16:27:26 +0100

    [FIX] General Refactoring

--- stock_analytic_reserve/models/stock_inventory.py
+++ stock_analytic_reserve/models/stock_inventory.py
@@ -5,62 +5,8 @@
 from openerp import api, fields, models, _
 
 
-class StockInventoryLine(models.Model):
-    _inherit = "stock.inventory.line"
-
-    def _get_quants(self, cr, uid, line, context=None):
-        quant_obj = self.pool["stock.quant"]
-        dom = [('company_id', '=', line.company_id.id), ('location_id', '=', line.location_id.id), ('lot_id', '=', line.prod_lot_id.id),
-                        ('product_id','=', line.product_id.id), ('owner_id', '=', line.partner_id.id), ('package_id', '=', line.package_id.id),
-                        ('analytic_account_id', '=', line.analytic_account_id.id)]
-        quants = quant_obj.search(cr, uid, dom, context=context)
-        return quants
-
-
-    @api.onchange('product_id', 'location_id', 'product_uom_id',
-                  'analytic_account_id', 'location_id', 'package_id',
-                  'partner_id')
-    def onchange_line(self):
-        quant_obj = self.env["stock.quant"]
-        uom_obj = self.env["product.uom"]
-        res = {'value': {}}
-        # If no UoM already put the default UoM of the product
-        if self.product_id:
-            product = self.product_id
-            uom = self.product_uom_id
-            if product.uom_id.category_id.id != uom.category_id.id:
-                self.product_uom_id = product.uom_id.id
-                res['domain'] = {
-                    'product_uom_id': [('category_id', '=',
-                                        product.uom_id.category_id.id)]}
-        # Calculate theoretical quantity by searching the quants as in
-        # quants_get
-        if self.product_id and self.location_id:
-            product = self.product_id
-            company_id = self.env['res.users'].browse(self._uid).\
-                company_id.id
-            dom = [('company_id', '=', company_id),
-                   ('location_id', '=', self.location_id.id),
-                   ('lot_id', '=', self.prod_lot_id.id),
-                   ('product_id', '=', self.product_id.id),
-                   ('owner_id', '=', self.partner_id.id),
-                   ('package_id', '=', self.package_id.id),
-                   ('analytic_account_id', '=', self.analytic_account_id.id)]
-            quants = quant_obj.search(dom)
-            th_qty = sum([x.qty for x in quants])
-            if self.product_id and self.product_uom_id and\
-                product.uom_id.id != self.product_uom_id:
-                    th_qty = uom_obj._compute_qty(product.uom_id.id, th_qty,
-                                              self.product_uom_id.id)
-            self.theoretical_qty = th_qty
-            self.product_qty = th_qty
-
-
 class StockInventory(models.Model):
     _inherit = "stock.inventory"
-
-    analytic_account_id = fields.Many2one(
-            'account.analytic.account', 'Analytic account')
 
     @api.multi
     def onchange_filter(self, filter):
