50ee73f453e5ce52ebd092dd71c6e2c2edff7ae6
ahenriquez <ahenriquez@eficent.com>
2016-12-07 12:48:12 +0100

    [FIX] When writing analytic_account only set timesheet flag if it is a work package

--- analytic_stage_use_timesheets/models/account_analytic_account.py
+++ analytic_stage_use_timesheets/models/account_analytic_account.py
@@ -9,13 +9,17 @@ class AccountAnalyticAccount(orm.Model):
     _inherit = 'account.analytic.account'
 
     def write(self, cr, uid, ids, values, context=None):
-        if values.get('stage_id', False):
-            stage_obj = self.pool['analytic.account.stage']
-            stage = stage_obj.browse(cr, uid, values['stage_id'],
-                                     context=context)
-            if stage.use_timesheets:
-                values['use_timesheets'] = True
-            else:
+        for project_id in ids:
+            project = self.browse(cr, uid, project_id)
+            if values.get('stage_id', False):
+                stage_obj = self.pool['analytic.account.stage']
+                stage = stage_obj.browse(cr, uid, values['stage_id'],
+                                         context=context)
+                if stage.use_timesheets and project.account_class == \
+                        'work_package':
+                    values['use_timesheets'] = True
+                else:
+                    values['use_timesheets'] = False
                 values['use_timesheets'] = False
         return super(AccountAnalyticAccount, self).write(cr, uid, ids,
                                                          values,
