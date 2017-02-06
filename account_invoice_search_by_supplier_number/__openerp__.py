5a62e9db6818251126e5b716a9e9ce5a6add1ef0
ahenriquez <ahenriquez@eficent.com>
2017-02-06 17:16:50 +0100

    [ADD] account_invoice_search_by_supplier_number

---
+++ account_invoice_search_by_supplier_number/__openerp__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# © 2015 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Account invoice search by supplier number",
+    "version": "7.0.1.0.0",
+    "author": "Eficent Business and IT Consulting Services S.L.",
+    "category": "Invoicing",
+    "website": "http://www.eficent.com/",
+    "license": "AGPL-3",
+    "description": """
+Search invoices by supplier invoice number
+===============================================
+Module features:
+    - add the invoice supplier number when searching invoices
+
+    """,
+    "depends": [
+        "account"
+    ], 
+    "demo": [], 
+    "data": [],
+    "installable": True, 
+    "auto_install": False, 
+    "active": False
+}
