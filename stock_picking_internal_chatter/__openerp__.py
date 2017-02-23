788953e840aa3887e1653646f04e5dcd64e529f3
aheficent <ahenriquez@eficent.com>
2017-02-23 16:35:02 +0100

    [ADD] Adds the chatter to internal pickings

---
+++ stock_picking_internal_chatter/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# © 2017 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Adds the chatter to internal pickings",
+    "version": "7.0.1.0.0",
+    "author": "Eficent Business and IT Consulting Services S.L.",
+    "category": "Purchases",
+    "website": "http://www.eficent.com/",
+    "license": "AGPL-3", 
+    "depends": [
+        "stock"
+    ], 
+    "demo": [], 
+    "data": [
+        "view/stock_picking_view.xml"
+    ],
+    "installable": True, 
+    "auto_install": False, 
+    "active": False
+}
