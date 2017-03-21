1370a4a5f1e0b6baa307995d15c07c1c6ca8e1f0
aheficent <ahenriquez@eficent.com>
2017-03-21 11:31:39 +0100

    [ADD] account_analytic_post_by_type

---
+++ account_analytic_post_by_type/__openerp__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# © 2016 - Eficent http://www.eficent.com/
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Account Analytic Post by Type',
+    'version': '8.0.1.0.0',
+    'category': 'Analytic Accounting',
+    'license': 'AGPL-3',
+    'author': "Eficent",
+    'website': 'http://www.eficent.com/',
+    'depends': ['account'],
+    'installable': True,
+    'auto_install': False,
+}
