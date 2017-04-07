28978ebdae3bc7b28383b2499e5387c2ef3bbc8c
lreficent <lois.rilo@eficent.com>
2017-04-07 17:58:01 +0200

    hr_timesheet_sheet_import_accounts

--- hr_timesheet_sheet_import_accounts/models/hr_timesheet_sheet.py
+++ hr_timesheet_sheet_import_accounts/models/hr_timesheet_sheet.py
@@ -1,41 +1,24 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2014-17 Eficent Business and IT Consulting Services, S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
-from openerp.osv import orm, fields
-from openerp import SUPERUSER_ID
+from openerp import api, fields, models
+from openerp.tools import DEFAULT_SERVER_DATE_FORMAT
 
 
-class HrTimesheetSheet(orm.Model):
+class HrTimesheetSheet(models.Model):
     _inherit = "hr_timesheet_sheet.sheet"
 
-    def set_previous_timesheet_ids(self, cr, uid, ids, context=None):
-
-        sheet_obj = self.pool.get('hr_timesheet_sheet.sheet')
-        timesheet_obj = self.pool.get('hr.analytic.timesheet')
-
-        for sheet in sheet_obj.browse(cr, SUPERUSER_ID, ids, context=context):
-            date_from = datetime.strptime(sheet.date_from, '%Y-%m-%d')
-            user = self.pool.get('res.users').browse(cr, uid, uid, context=context)
+    @api.multi
+    def set_previous_timesheet_ids(self):
+        sheet_obj = self.env['hr_timesheet_sheet.sheet']
+        timesheet_obj = self.env['account.analytic.line']
+        for sheet in self:
+            date_from = datetime.strptime(sheet.date_from,
+                                          DEFAULT_SERVER_DATE_FORMAT)
+            user = self.env.user
             r = user.company_id and user.company_id.timesheet_range or 'month'
             delta = relativedelta(months=-1)
             if r == 'month':
@@ -44,70 +27,41 @@ class HrTimesheetSheet(orm.Model):
                 delta = relativedelta(weeks=-1)
             elif r == 'year':
                 delta = relativedelta(years=-1)
-            date_from_lw = (date_from + delta).strftime('%Y-%m-%d')
+            date_from_lw = (date_from + delta).strftime(
+                DEFAULT_SERVER_DATE_FORMAT)
             emp_id = sheet.employee_id and sheet.employee_id.id or False
             if not emp_id:
                 return False
-            ga_id = sheet.employee_id.product_id.\
-                property_account_expense.id
-            if not ga_id:
-                ga_id = sheet.employee_id.product_id.\
-                    categ_id.property_account_expense_categ.id
-
-            lw_sheet_ids = sheet_obj.search(
-                cr, uid, [('date_to', '>=', date_from_lw),
-                          ('date_from', '<=', date_from_lw),
-                          ('employee_id', '=', emp_id)],
-                context=context)
-            a_line_ids = []
-            for lw_sheet in sheet_obj.browse(cr, uid, lw_sheet_ids,
-                                             context=context):
-                for timesheet_id in lw_sheet.timesheet_ids:
-                    if timesheet_id.line_id:
-                        a_line_ids.append(timesheet_id.line_id.id)
+            lw_sheet_ids = sheet_obj.search([
+                ('date_to', '<=', date_from),
+                ('date_from', '>=', date_from_lw),
+                ('employee_id', '=', emp_id)])
+            a_line_ids = lw_sheet_ids.mapped('timesheet_ids').ids
             if a_line_ids:
-                cr.execute("""SELECT DISTINCT account_id
+                self.env.cr.execute("""SELECT DISTINCT account_id
                 FROM account_analytic_line AS L
                 INNER JOIN account_analytic_account as A
                 ON A.id = L.account_id
                 WHERE L.id IN %s
-                AND A.use_timesheets = True
+                AND L.is_timesheet = True
                 GROUP BY account_id""", (tuple(a_line_ids),))
-                res = cr.dictfetchall()
-                timesheet_ids = []
-                for res_vals in res:
-                    same_account_id = False
-                    for timesheet_id in sheet.timesheet_ids:
-                        if (
-                            timesheet_id.line_id and
-                            timesheet_id.line_id.account_id and
-                            timesheet_id.line_id.account_id.id ==
-                                res_vals['account_id']
-                        ):
-                            same_account_id = True
-                    if same_account_id is True:
-                        continue
+                res = self.env.cr.dictfetchall()
+                for account_id in [a['account_id'] for a in res]:
                     vals = {
                         'employee_id': sheet.employee_id.id,
-                        'journal_id': sheet.employee_id.journal_id.id,
+                        # 'journal_id': sheet.employee_id.journal_id.id,
                         'date': sheet.date_from,
-                        'account_id': res_vals['account_id'],
+                        'account_id': account_id,
                         'name': '/',
-                        'product_id': sheet.employee_id.product_id.id,
-                        'product_uom_id':
-                            sheet.employee_id.product_id.uom_id.id,
-                        'general_account_id': ga_id,
-                        'user_id': context.get('user_id') or uid,
+                        # 'product_id': sheet.employee_id.product_id.id,
+                        # 'product_uom_id':
+                        #     sheet.employee_id.product_id.uom_id.id,
+                        # TODO: is a general account needed? general_account_id
+                        # TODO: is now related='move_id.account_id'.
+                        # 'general_account_id': ga_id,
+                        'user_id': self.env.uid,
+                        'sheet_id': sheet.id,
+                        'is_timesheet': True,
                     }
-
-                    timesheet_id = timesheet_obj.create(cr, uid, vals,
-                                                        context=context)
-                    timesheet_ids.append(timesheet_id)
-
-                if timesheet_ids:
-                    sheet_obj.write(cr, uid, sheet.id,
-                                    {'timesheet_ids': [(4, timesheet_id)
-                                                       for timesheet_id
-                                                       in timesheet_ids]})
-
-        return False
+                    timesheet_obj.create(vals)
+        return True
