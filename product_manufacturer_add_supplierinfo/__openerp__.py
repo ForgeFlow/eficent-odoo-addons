eb4af33a914ffca05f91b15ca5f78a9a42a29c89
gmeficent <gisela.mora@eficent.com>
2017-04-10 15:26:47 +0200

    [MIG][9.0]product_manufacturer_add_supplierinfo

---
+++ product_manufacturer_add_supplierinfo/__openerp__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# © 2015-17 Eficent Business and IT Consulting Services S.L.
+# - Jordi Ballester Alomar
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Product manufacturer add supplier info",
+    "summary": "The manufacturer indicated in the product is automatically "
+               "added as a vendor in the supplier info.",
+    "version": "9.0.1.0.0",
+    "author": "Eficent Business and IT Consulting Services S.L",
+    "website": "www.eficent.com",
+    "category": "Purchase Management",
+    "depends": ["product", "product_manufacturer"],
+    'installable': True,
+}
