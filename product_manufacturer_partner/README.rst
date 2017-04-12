6a0ed460386a52498a0755ed9551532d4b80d025
gmeficent <gisela.mora@eficent.com>
2017-04-12 10:27:50 +0200

    [MIG][9.0]product_manufacturer_partner

--- product_manufacturer_partner/README.rst
+++ product_manufacturer_partner/README.rst
@@ -2,9 +2,9 @@
    :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
    :alt: License: AGPL-3
 
-==============
+============================
 Product manufacturer partner
-==============
+============================
 
 This module helps to better classify manufacturers by providing an indicator
 in the partner.
--- product_manufacturer_partner/__openerp__.py
+++ product_manufacturer_partner/__openerp__.py
@@ -1,23 +1,6 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2014-17 Eficent (<http://www.eficent.com/>)
-#             <contact@eficent.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2015 Eficent Business and IT Consulting Services S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 {
     'name': 'Products Manufacturer Partner',
     'version': '9.0.1.0',
@@ -25,16 +8,11 @@
     "website": "www.eficent.com",
     'category': 'Purchase Management',
     'depends': ['purchase', 'product_manufacturer'],
-    'description': """
-A module that classifies partners as manufacturers
-==================================================
-This module helps to better classify manufacturers by providing an indicator
-in the partner.
-
-In the product, a user can only select a manufacturer from the list of partners
-where this indicator has been set.
-
-    """,
+    'summary': """
+    Classify manufacturers by providing an indicator in the partner.
+    In the product, select a manufacturer from the list of partners where
+    this indicator has been set.
+     """,
     'data': [
         'view/partner_view.xml',
         'view/product_view.xml',
