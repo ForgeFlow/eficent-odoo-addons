da13499a8fb729c28f04fce49bd28d44f211d612
gmeficent <gisela.mora@eficent.com>
2017-04-12 13:37:35 +0200

    [9.0][MIG]product_manufacturer_partner

---
+++ product_manufacturer_partner/model/product.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# © 2015-17 Eficent Business and IT Consulting Services S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+from openerp import models, fields
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    manufacturer = fields.Many2one('res.partner', 'Manufacturer',
+                                   domain=[('manufacturer', '=', True)])
