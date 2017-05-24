2ae18989bf4add536a86b6a8a0d4cbb3b7ce5da8
gmeficent <gisela.mora@eficent.com>
2017-05-24 15:56:30 +0200

    [ADD][10.0]CB_demo_database

---
+++ CB_demo_database/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016-2017 LasLabs Inc.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name': 'Creu Blanca Demo Database',
+    'summary': '',
+    'version': '10.0.1.0.0',
+    'author': 'Eficent, Odoo Community Association (OCA)',
+    'category': '',
+    'depends': [
+        'medical_careplan',
+    ],
+    'demo': [
+        'demo/careplan_demo.xml',
+    ],
+    'website': '',
+    'licence': 'LGPL-3',
+    'installable': True,
+    'auto_install': False,
+}
