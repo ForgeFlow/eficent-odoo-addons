f1ac9fb34707d22b9ce1f0cc233c3bba69f01414
aheficent <ahenriquez@eficent.com>
2017-06-29 18:24:10 +0200

    [MIG]stock_location_analytic_purchase to v9

---
+++ stock_location_analytic_purchase/__openerp__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# © 2017 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Purchase Stock Location Analytic',
+    'version': '9.0.1.0.0',
+    'category': 'Analytic',
+    'summary': 'Purchases using analytic locations',
+    'author': 'Eficent',
+    'website': 'http://www.eficent.com',
+    'depends': ['stock_analytic_account_location', 'purchase',
+                'purchase_stock_analytic'],
+    'installable': True,
+    'auto_install': False,
+}
