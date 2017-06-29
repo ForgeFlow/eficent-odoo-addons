de24e5cb11ffb7ee984709d700711c5a9c53db73
aheficent <ahenriquez@eficent.com>
2017-06-29 16:26:43 +0200

    [MIG]stock_analytic_change_product_qty to v9

---
+++ stock_location_analytic_change_product_qty/__openerp__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# © 2017 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Update Product Qty Analytic',
+    'version': '9.0.1.0.0',
+    'category': 'Analytic',
+    'summary': 'Update product Qty when using Analytic locations',
+    'author': 'Eficent',
+    'website': 'http://www.eficent.com',
+    'depends': ['analytic', 'stock', 'stock_analytic_account_location'],
+    'data': ['wizard/stock_change_product_qty.xml'],
+    'installable': True,
+    'auto_install': False,
+}
