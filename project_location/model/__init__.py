b6a2b9bc6a4ff64f54adb992d0b0d6214c96c525
aheficent <ahenriquez@eficent.com>
2017-07-04 17:20:22 +0200

    [MIG]project_location to v9

--- project_location/model/__init__.py
+++ project_location/model/__init__.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
 # © 2017 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from . import stock_location
-from . import stock_quant
-from . import analytic_account
+from . import project
