55f960d1b61937af9de4cf437cdd71f365bf8d49
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2017-08-09 16:22:30 +0530

    [MIG] hr_timesheet_sheet_validators v10

--- hr_timesheet_sheet_validators/__init__.py
+++ hr_timesheet_sheet_validators/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright 2014-17 Eficent Business and IT Consulting Services, S.L.
+# Copyright 2017 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import models
---
+++ hr_timesheet_sheet_validators/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2014-17 Eficent Business and IT Consulting Services, S.L.
+# Copyright 2017 Serpent Consulting Services Pvt. Ltd.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'HR Timesheet Sheet Validators',
+    'version': '10.0.1.0.0',
+    'author': 'Eficent, Serpent Consulting Services Pvt. Ltd.',
+    'category': 'Human Resources',
+    'summary': """This module allows a user outside of the Human Resources
+               groups to validate timesheets. A rule is predefined, but it
+               is flexible enough to accept extensions.""",
+    'website': 'http://www.eficent.com',
+    'depends': [
+        'hr_timesheet_sheet'
+    ],
+    'data': [
+        'security/hr_timesheet_sheet_security.xml',
+        'views/hr_timesheet_sheet_view.xml',
+    ],
+    'installable': True,
+}
