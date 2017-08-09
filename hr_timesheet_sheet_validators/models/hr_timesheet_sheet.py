f15b5b772bbd9b8130027ce2773193d94dfeb630
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2017-08-09 16:22:30 +0530

    [MIG] hr_timesheet_sheet_validators v10

--- hr_timesheet_sheet_validators/models/hr_timesheet_sheet.py
+++ hr_timesheet_sheet_validators/models/hr_timesheet_sheet.py
@@ -1,17 +1,14 @@
 # -*- coding: utf-8 -*-
 # Copyright 2014-17 Eficent Business and IT Consulting Services, S.L.
+# Copyright 2017 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import api, fields, models, _
-from openerp import netsvc
-from openerp.exceptions import UserError
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
 
 
 class HrTimesheetSheet(models.Model):
     _inherit = "hr_timesheet_sheet.sheet"
-
-    validator_user_ids = fields.Many2many(
-        'res.users', string='Validators')
 
     @api.model
     def _default_department(self):
@@ -21,9 +18,16 @@ class HrTimesheetSheet(models.Model):
             return emp.department_id and emp.department_id.id or False
         return False
 
-    _defaults = {
-        'department_id': _default_department,
-    }
+    validator_user_ids = fields.Many2many(
+        'res.users',
+        string='Validators'
+    )
+
+    department_id = fields.Many2one(
+        comodel_name='hr.department',
+        string='Department',
+        default=_default_department,
+    )
 
     @api.multi
     def _get_validator_user_ids(self):
@@ -45,22 +49,22 @@ class HrTimesheetSheet(models.Model):
                     and timesheet.department_id.parent_id.manager_id
                     and timesheet.department_id.parent_id.manager_id.user_id
                     and timesheet.department_id.parent_id.manager_id.
-                        user_id.id != self.env.uid):
+                    user_id.id != self.env.uid):
                 users.append(
                     timesheet.department_id.manager_id.user_id.id)
             return list(set(users)) if users else []
 
     @api.multi
-    def button_confirm(self):
+    def action_timesheet_confirm(self):
         for sheet in self:
             validators = sheet._get_validator_user_ids()
             sheet.write({'validator_user_ids': [(6, 0, validators)]})
-        return super(HrTimesheetSheet, self).button_confirm()
+        return super(HrTimesheetSheet, self).action_timesheet_confirm()
 
     @api.multi
     def _check_authorised_validator(self):
-        group_hr_manager = self.env.ref('base.group_hr_manager')
-        group_hr_user = self.env.ref('base.group_hr_user')
+        group_hr_manager = self.env.ref('hr.group_hr_manager')
+        group_hr_user = self.env.ref('hr.group_hr_user')
         for timesheet in self:
             if group_hr_manager and self.env.uid in group_hr_manager.users.ids:
                 continue
@@ -75,24 +79,11 @@ class HrTimesheetSheet(models.Model):
                                   'refuse this Timesheet.'))
 
     @api.multi
-    def action_set_to_draft(self):
+    def action_timesheet_draft(self):
         self._check_authorised_validator()
-        return super(HrTimesheetSheet, self).action_set_to_draft()
+        return super(HrTimesheetSheet, self).action_timesheet_draft()
 
     @api.multi
-    def action_done(self):
+    def action_timesheet_done(self):
         self._check_authorised_validator()
-        wf_service = netsvc.LocalService('workflow')
-        for id in self.ids:
-            wf_service.trg_validate(self.env.uid, 'hr_timesheet_sheet.sheet',
-                                    id, 'done', self.env.cr)
-        return True
-
-    @api.multi
-    def action_cancel(self):
-        self._check_authorised_validator()
-        wf_service = netsvc.LocalService('workflow')
-        for id in self.ids:
-            wf_service.trg_validate(self.env.uid, 'hr_timesheet_sheet.sheet',
-                                    id, 'cancel', self.env.cr)
-        return True
+        return super(HrTimesheetSheet, self).action_timesheet_done()
--- hr_timesheet_sheet_validators/tests/__init__.py
+++ hr_timesheet_sheet_validators/tests/__init__.py
@@ -3,4 +3,4 @@
 # Copyright 2017 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import hr_timesheet_sheet
+from . import test_hr_timesheet_sheet_validators
