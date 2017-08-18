25d55e91671bf8159a0a8812ca5d0764ccabaef0
aheficent <ahenriquez@eficent.com>
2017-08-18 15:37:24 +0200

    [MIG]analytic_wip_report_fiscalyear to v9

--- analytic_wip_report_fiscalyear/__openerp__.py
+++ analytic_wip_report_fiscalyear/__openerp__.py
@@ -1,50 +1,25 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2014 Eficent (<http://www.eficent.com/>)
-#               <contact@eficent.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
+# © 2014-17 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Work In Progress Statement including fiscal year totals",
-    "version": "1.0",
+    "version": "9.0.1.0.0",
     "author": "Eficent",
     "website": "",
+    "license": "LGPL-3",
     "category": "Generic Modules/Projects & Services",
     "depends": [
                 "analytic_wip_report",
                 ],
-    "description": """
+    "summmary": """
     """,
 
-    "init_xml": [
-                ],
-    "update_xml": [
+    "data": [
         "analytic_wip_report.xml",
         "account_analytic_account_view.xml",
         "wizards/analytic_wip_report_view.xml"
     ],
-    'demo_xml': [
-
-    ],
-    'test':[
-    ],
     'installable': True,
-    'active': False,
-    'certificate': '',
+
 }
