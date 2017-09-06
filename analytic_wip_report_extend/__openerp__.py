ac00940e937a79d7f9a31342da4b9556a22984f1
aheficent <ahenriquez@eficent.com>
2017-09-06 13:16:44 +0200

    [MIG]analytic_wip_report_extend and analytic_wip_report_total to v9

---
+++ analytic_wip_report_extend/__openerp__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# © 2016 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+
+{
+    "name": "Work In Progress Statement Extend",
+    "version": "9.0.1.0.0",
+    "author": "Eficent Business and IT Consulting Services S.L.",
+    "category": "Projects",
+    "summary": """
+        This adds new fields to analytic wip statement report
+
+""",
+    "website": "http://www.eficent.com/",
+    "license": "",
+    "depends": [
+        "analytic_wip_report",
+    ],
+    "data": [
+        "account_analytic_account_view.xml"
+    ],
+    "installable": True,
+    "auto_install": False,
+    "active": False
+}
