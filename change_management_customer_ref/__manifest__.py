5e31771e87bec74169179ca37c3a1f229755df88
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2017-09-08 17:26:10 +0530

    [MIG] change_management_customer_ref v10

--- change_management_customer_ref/__manifest__.py
+++ change_management_customer_ref/__manifest__.py
@@ -1,20 +1,23 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Change Management Customer Reference',
-    'version': '9.0.1.0.0',
+    'version': '10.0.1.0.0',
     'author':   'Eficent Business and IT Consulting Services S.L.',
     'website': 'www.eficent.com',
     'category': 'Project Management',
     'license': 'AGPL-3',
     'summary': 'Creates the customer and Change code',
-    'depends': ['change_management'],
+    'depends': [
+        'change_management'
+    ],
     'data': [
         'views/change_management_view.xml',
     ],
-    'installable': False,
+    'installable': True,
     'application': True,
-    'active': False,
+    'active': True,
 }
