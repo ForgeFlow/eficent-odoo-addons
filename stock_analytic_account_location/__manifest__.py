043c5b53bedc8974eafc5bcf086ad05c2e43844f
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2017-09-14 15:46:47 +0530

    [MIG] stock_analytic_account_location v10

---
+++ stock_analytic_account_location/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017 Serpent Consulting Services Pvt. Ltd.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Stock Analytic Account Location',
+    'version': '10.0.1.0.0',
+    'category': 'Logistics',
+    'summary': 'Stock analytic account using analytic locations',
+    'author': 'Eficent',
+    'website': 'http://www.eficent.com',
+    'depends': [
+        'stock',
+        'analytic',
+        'account_analytic_parent',
+        'stock_location_analytic'
+    ],
+    'installable': True,
+}
