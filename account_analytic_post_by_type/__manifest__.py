46f30ff8ec3a5b33a4350033827226fbd0e22ce6
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2017-09-16 12:41:01 +0530

    [MIG] account_analytic_post_by_type v10

---
+++ account_analytic_post_by_type/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017 Serpent Consulting Services Pvt. Ltd.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Account Analytic Post by Type',
+    'version': '10.0.1.0.0',
+    'category': 'Analytic Accounting',
+    'license': 'AGPL-3',
+    'author': 'Eficent Business and IT Consulting Services S.L.',
+    'website': 'http://www.eficent.com',
+    'depends': [
+        'account'
+    ],
+    'installable': True,
+    'auto_install': False,
+}
