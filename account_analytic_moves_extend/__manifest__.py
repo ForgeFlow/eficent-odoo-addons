fc2acb40a7ded63234b60680ab05a3222b1637a8
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2017-09-19 15:23:14 +0530

    [MIG] account_analytic_moves_extend v10

---
+++ account_analytic_moves_extend/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Analytic moves only for expenses and revenues",
+    "version": "10.0.1.0.0",
+    "author": "Eficent",
+    "license": 'AGPL-3',
+    "website": "www.eficent.com",
+    "description": """Limits the creation of analytic lines associated to
+        invoices accepted only when the move is associated to an expense or
+        revenue account.
+    """,
+    "depends": [
+        "account",
+        "analytic"
+    ],
+    "installable": True,
+}
---
+++ account_analytic_moves_extend/model/account_move_line.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import api, models, _
+from odoo.exceptions import UserError
+
+
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
+
+    @api.multi
+    def create_analytic_lines(self):
+        acc_ana_line_obj = self.env['account.analytic.line']
+        for obj_line in self:
+            # Only create analytic line if the associated account is
+            # Expense or Income.
+            if obj_line.account_id.user_type_id.name and\
+                    obj_line.account_id.user_type_id.type in ('Income',
+                                                              'Expenses'):
+                if obj_line.analytic_account_id:
+                    if obj_line.analytic_line_ids:
+                        super(AccountMoveLine, self).create_analytic_lines()
+        return True
