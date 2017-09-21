b2daf9158fd90987527a1a7c73fca424e2126076
aheficent <ahenriquez@eficent.com>
2017-09-21 17:49:53 +0200

    [MIG]project_stage_use_timesheets to v9

---
+++ project_stage_use_timesheets/README.rst
@@ -0,0 +1,26 @@
+.. image:: https://img.shields.io/badge/license-AGPLv3-blue.svg
+   :target: https://www.gnu.org/licenses/agpl.html
+   :alt: License: AGPL-3
+
+=============================
+Analytic Stage Use Timesheets
+=============================
+
+This module defaults the Use Timesheets field depending on the stage of
+the project/analytic account.
+
+
+Usage
+=====
+
+* For each stage, set or clear the field 'Timesheets'
+
+
+Credits
+=======
+
+Contributors
+------------
+
+* Jordi Ballester Alomar <jordi.ballester@eficent.com>
+* Aaron Henriquez <ahenriquez@eficent.com>
---
+++ project_stage_use_timesheets/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © 2015-17 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Project Stage Use Timesheets",
+    "version": "10.0.1.0.0",
+    "summary": "Defaults the Use Timesheets field depending on the stage of "
+               "the project/analytic account",
+    "author": "Eficent",
+    "website": "http://www.eficent.com",
+    "category": "Generic",
+    "depends": ["project_wbs_stage", "hr_timesheet"],
+    "license": "AGPL-3",
+    "data": [
+        "views/project_project_stage_view.xml",
+    ],
+    'installable': True,
+}
