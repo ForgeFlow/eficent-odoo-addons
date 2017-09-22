b9c17886d98d16213d27a76276c45b99def5f3ee
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2017-09-22 17:18:02 +0530

    [MIG] project_location v10

---
+++ project_location/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# © 2017 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Stock Location in Project",
+    "version": "10.0.1.0.0",
+    "author": "Eficent",
+    "license": 'AGPL-3',
+    "website": "www.eficent.com",
+    "category": "Generic Modules/Projects & Services",
+    "Summary": """stock location in the project.""",
+    "depends": [
+        "project_stock",
+        "analytic_location"
+    ],
+    "data": [
+        "view/project_view.xml",
+        "security/ir.model.access.csv",
+    ],
+}
