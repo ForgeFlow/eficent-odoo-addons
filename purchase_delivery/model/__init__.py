df95d5a2300b300092a217cf4441169f27136c31
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2017-10-23 16:26:11 +0530

    [MIG] purchase_delivery v10

---
+++ purchase_delivery/model/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Copyright 2015-17 Eficent Business and IT Consulting Services S.L.
+#        <contact@eficent.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from . import delivery
+from . import purchase
+from . import stock
