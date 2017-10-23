1bb5039135a44ed95d7bee1e8c08afd25d40f1ec
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2017-10-23 16:26:11 +0530

    [MIG] purchase_delivery v10

---
+++ purchase_delivery/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright 2015-17 Eficent Business and IT Consulting Services S.L.
+#        <contact@eficent.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Purchase Delivery Costs",
+    "version": "10.0.1.0.0",
+    "category": "Purchase Management",
+    "description": """Allows you to add delivery methods in purchase orders
+        and picking.""",
+    "author": "Eficent",
+    "website": "www.eficent.com",
+    "license": "AGPL-3",
+    "depends": [
+        "delivery",
+        "purchase"
+    ],
+    "data": [
+        "view/delivery_view.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
