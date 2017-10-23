e6834f40d4b8dfd2e430f3a3107822885dd9f65a
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2017-10-23 17:27:24 +0530

    [MIG] delivery_no_weight_unit v10

---
+++ delivery_no_weight_unit/README.rst
@@ -0,0 +1,17 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+    :alt: License AGPL-3
+
+=======================
+Delivery No Weight Unit
+=======================
+Delivery No Weight Unit
+
+Credits
+=======
+
+Contributors
+------------
+
+* Eficent <http://www.eficent.com>
+* Serpent Consulting Services Pvt. Ltd. <support@serpentcs.com>
+
---
+++ delivery_no_weight_unit/migrations/7.0.1.0.2/post-migration.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright 2011-17 Camptocamp SA
+# Author: Nicolas Bessi.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+
+def migrate(cr, version):
+    if not version:
+        # only run at first install
+        cr.execute("UPDATE stock_move "
+                   " SET weight_uom_id = NULL;")
+
+        cr.execute("UPDATE stock_picking"
+                   " SET weight_uom_id = NULL;")
