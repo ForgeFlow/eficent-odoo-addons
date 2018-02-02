3edd870595eae2c135efccaa60fd2cce80157f97
aheficent <ahenriquez@eficent.com>
2018-02-02 15:01:41 -0500

    [FIX]

--- stock_analytic_account_location/models/stock_move.py
+++ stock_analytic_account_location/models/stock_move.py
@@ -90,12 +90,12 @@ class StockMove(orm.Model):
 
     def _check_analytic(self, cr, uid, ids, context=None):
         for move in self.browse(cr, uid, ids):
-            if move.analytic_account_id and move.location_dest_id:
-                if move.location_dest_id.analytic_account_id:
-                    if move.analytic_account_id.id != move.location_dest_id.analytic_account_id.id and not move.location_id.analytic_account_id:
-                        return False
-            if move.analytic_account_id and not move.location_dest_id.analytic_account_id and not move.location_id.analytic_account_id:
-                return False
+            if move.analytic_account_id:
+                if ((move.location_id.analytic_account_id !=
+                        move.analytic_account_id)
+                    or (move.location_dest_id.analytic_account_id !=
+                        move.analytic_account_id)):
+                    return False
         return True
 
     _constraints = [(_check_analytic_account,
