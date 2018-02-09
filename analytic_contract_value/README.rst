5b057a4c6e8b6a13d9ac3206b112811ced86db7f
aheficent <ahenriquez@eficent.com>
2018-02-09 17:26:36 +0100

    [MIG]analytic_contract_value to v10

---
+++ analytic_contract_value/README.rst
@@ -0,0 +1,19 @@
+.. image:: https://img.shields.io/badge/license-AGPLv3-blue.svg
+   :target: https://www.gnu.org/licenses/agpl.html
+   :alt: License: AGPL-3
+
+=======================
+Analytic Contract VAlue
+=======================
+
+It allows to set the manual contract value, despite the planning lines
+
+
+Credits
+=======
+
+Contributors
+------------
+
+* Jordi Ballester Alomar <jordi.ballester@eficent.com>
+* Aaron Henriquez <ahenriquez@eficent.com>
---
+++ analytic_contract_value/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# © 2015 Eficent - Jordi Ballester Alomar
+# © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Analytic Contract Value",
+    "version": "7.0.1.0.0",
+    "summary": "Sets a Contract Value on the analytic account",
+    "author": "Eficent",
+    "website": "http://www.eficent.com",
+    "category": "Generic",
+    "depends": ["analytic"],
+    "license": "AGPL-3",
+    "data": [
+        "security/analytic_security.xml",
+        "wizards/analytic_change_contract_value_view.xml",
+        "views/analytic_account_view.xml",
+
+    ],
+    'installable': True,
+    'active': False,
+}
