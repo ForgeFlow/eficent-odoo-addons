79581f342d914fe25116e30b34a67a36a938367d
aheficent <ahenriquez@eficent.com>
2018-02-22 18:32:43 +0100

    [MIG]account_move_category [FIX]wip st

---
+++ account_move_category/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Account Move Category",
+    "version": "10.0.1.0.0",
+    "author": "Eficent, Odoo Community Association (OCA)",
+    "website": "http://www.eficent.com",
+    "category": "Generic",
+    "depends": ["account", "analytic"],
+    "summary": """
+        Account Move Category
+    """,
+    "license": "AGPL-3",
+    "data": [
+        "views/account_move_view.xml",
+    ],
+    'installable': True,
+}
