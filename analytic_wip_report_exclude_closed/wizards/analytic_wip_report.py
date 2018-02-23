56ab8d9918b71380f59f629cad914953fc258cf1
aheficent <ahenriquez@eficent.com>
2018-02-23 13:59:33 +0100

    [ADD]analytic_wip_report_contract_value [FIX]wip st

--- analytic_wip_report_exclude_closed/wizards/analytic_wip_report.py
+++ analytic_wip_report_exclude_closed/wizards/analytic_wip_report.py
@@ -14,38 +14,34 @@ class AnalyticWipReport(models.TransientModel):
     )
 
     @api.multi
+    def analytic_wip_report_open_window(self):
+        res = super(AnalyticWipReport, self).analytic_wip_report_open_window()
+        res['domain'] = self._get_analytic_search_domain()
+        return res
+
+    @api.multi
     def _get_analytic_search_domain(self):
         comparing_date = self.from_date
-        project_stage = self.env['project.project.stage'].\
-            search([('name', 'ilike', 'close')]).mapped('id') or []
+        project_stage = self.env['analytic.account.stage'].\
+            search([('name', 'in', ('Closed', 'Cancelled'))]).mapped('id') or []
         if self.filter_project and not comparing_date:
-            project_ids = self.env['project.project'].\
+            project_ids = self.env['account.analytic.account'].\
                 search([('account_class', '=', 'project'),
                         ('stage_id', 'not in', project_stage)])
-            domain = [('project_ids', 'in', project_ids.ids)] or []
+            domain = [('id', 'in', project_ids.ids)] or []
         elif self.filter_project and comparing_date:
-            project_ids = self.env['project.project'].\
+            project_ids = self.env['account.analytic.account'].\
                 search([('date_start', '>=', comparing_date),
                         ('account_class', '=', 'project'),
                         ('stage_id', 'not in', project_stage)])
-            domain = [('project_ids', 'in', project_ids.ids)] or []
+            domain = [('id', 'in', project_ids.ids)] or []
         elif not self.filter_project and comparing_date:
-            project_ids = self.env['project.project'].\
+            project_ids = self.env['account.analytic.account'].\
                 search([('date_start', '>=', comparing_date),
                         ('stage_id', 'not in', project_stage)])
-            domain = [('project_ids', 'in', project_ids.ids)] or []
+            domain = [('id', 'in', project_ids.ids)] or []
         else:
-            project_ids = self.env['project.project'].\
+            project_ids = self.env['account.analytic.account'].\
                 search([('stage_id', 'not in', project_stage)])
-            domain = [('project_ids', 'in', project_ids.ids)] or []
+            domain = [('id', 'in', project_ids.ids)] or []
         return domain
-
-
-    @api.multi
-    def analytic_wip_report_open_window(self):
-        res = super(AnalyticWipReport, self).analytic_wip_report_open_window()
-        ctx = safe_eval(res['context'])
-        if not ctx.get('from_date', False):
-            return res
-        res['domain'] = self._get_analytic_search_domain()
-        return res
