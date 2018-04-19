2635f7e851601f3c29c838d18075169710868c00
aaron <ahenriquez@eficent.com>
2018-04-19 11:05:45 +0200

    [ADD]stock_inventory_negative

---
+++ stock_inventory_negative/__openerp__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# © 2017 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Stock Inventory Negative",
+    "version": "7.0.1.0.0",
+    "author": "Eficent",
+    "license": 'AGPL-3',
+    "website": "www.eficent.com",
+    'summary': 'Create an inventory of negative stock',
+    "depends": ["stock"],
+    'data': ['wizard/stock_fill_inventory_view.xml',
+    ],
+    'installable': True,
+}
