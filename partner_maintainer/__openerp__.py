9d0c2b92521e70119e578c0d0833949d97258668
aaron <ahenriquez@eficent.com>
2018-04-23 16:40:47 +0200

    [ADD]partner_maintainer 7.0

---
+++ partner_maintainer/__openerp__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Copyright Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Partner Maintainer",
+    "version": "1.0",
+    "author": "Eficent",
+    "website": "www.eficent.com",
+    "category": "Restrict Partner edition",
+    "depends": [
+        'base',
+    ],
+    "description": """
+Partner maintainer
+==================
+
+    """,
+    "data": [
+        "security/partner_maintainer_security.xml",
+        "security/ir.model.access.csv",
+    ],
+
+    'installable': True,
+    'active': False,
+    'certificate': '',
+}
