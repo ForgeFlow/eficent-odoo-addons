333cedbcaac74bed5ec95c614f6dfd42af706891
Aarón Henríquez <ahenriquez@eficent.com>
2018-05-22 13:29:12 +0200

    Revert "[FIX]Stock analytic account reserve"
    This reverts commit 0ae550157b2e969c345287c7b1b250fbfcf13bb2.

--- stock_analytic_account_reserve/model/__init__.py
+++ stock_analytic_account_reserve/model/__init__.py
@@ -18,5 +18,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
+from . import product
 from . import stock
+from . import stock_picking
 from . import analytic_account
