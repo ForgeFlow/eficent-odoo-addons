c8cf5f970c35e5c0bc4279bdc66f55730acc1f36
aaron <ahenriquez@eficent.com>
2018-06-18 13:47:38 +0200

    [ADD]analytic_resource_plan_stock_picking_operating_unit

---
+++ analytic_resource_plan_stock_picking_operating_unit/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Analytic Resource Planning - Operating Units",
+    "version": "10.0.1.0.0",
+    "author": "Eficent",
+    "license": 'AGPL-3',
+    "website": "www.eficent.com",
+    "description": """Fetch stock for projects""",
+    "category": "Generic Modules/Projects & Services",
+    "depends": [
+        "analytic_resource_plan_stock_picking",
+        "stock_operating_unit",
+    ],
+    'installable': True,
+}
