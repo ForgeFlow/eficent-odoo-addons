0dfe7e85901cbc36d777bfe685cf157bc8d1ca1b
aaron <ahenriquez@eficent.com>
2018-06-29 13:20:41 +0200

    [FIX] code RPL MRP [IMP] test RPL MRP

--- analytic_resource_plan_mrp/tests/test_analytic_resource_plan_mrp.py
+++ analytic_resource_plan_mrp/tests/test_analytic_resource_plan_mrp.py
@@ -31,13 +31,13 @@ class TestAnalyticResourcePlanMrp(
             'product_id': product.id,
             'product_tmpl_id': product.product_tmpl_id.id,
             'product_uom_id': product.uom_id.id,
-            'product_qty': 4.0,
+            'product_qty': 1.0,
             'type': 'normal',
         })
         cls.mrp_bom_line_obj.create({
             'bom_id': test_bom.id,
             'product_id': product.id,
-            'product_qty': 2,
+            'product_qty': 2.0,
         })
         return test_bom
 
@@ -54,7 +54,7 @@ class TestAnalyticResourcePlanMrp(
         return cls.analytic_resource_plan_obj.create({
             'name': product.name,
             'product_id': product.id,
-            'unit_amount': 10,
+            'unit_amount': 2.0,
             'product_uom_id': product.uom_id.id,
             'account_id': cls.account_id.id,
             'resource_type': 'procurement',
@@ -68,3 +68,21 @@ class TestAnalyticResourcePlanMrp(
             [('resource_plan_id', '=', child.id)])
         cls.assertEqual(len(plan_lines), 1, 'bad plan lines')
         cls.assertEqual(child.state, 'confirm', 'child not confirm')
+        cls.assertEqual(child.unit_amount, 4.0, 'wrong qty in child')
+        # test produce
+        wiz_id = cls.env['analytic.resource.plan.line.produce'].with_context(
+            active_model="analytic.resource.plan.line",
+            active_ids=cls.resource_plan_line_wbom_id.id).create({})
+        move_action = wiz_id.do_produce()
+        move_id = eval(move_action['domain'])[0][2]
+        move = cls.env['stock.move'].browse(move_id)[0]
+        cls.assertEqual(
+            move.product_qty, child.unit_amount, 'Wrong consumed qty')
+        # test consume
+        wiz_id = cls.env['analytic.resource.plan.line.consume'].with_context(
+            active_model="analytic.resource.plan.line",
+            active_ids=cls.resource_plan_line_wbom_id.id).create({})
+        move_action = wiz_id.do_consume()
+        move_id = eval(move_action['domain'])[0][2]
+        cls.assertEqual(
+            move.product_qty, child.unit_amount, 'Wrong consumed qty')
