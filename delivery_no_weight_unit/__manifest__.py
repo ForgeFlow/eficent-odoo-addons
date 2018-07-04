3e612a2518a57f8604d6db4704c0bb3fc7288135
aaron <ahenriquez@eficent.com>
2018-07-04 16:21:44 +0200

    [FIX]project percent completed

--- delivery_no_weight_unit/__manifest__.py
+++ delivery_no_weight_unit/__manifest__.py
@@ -6,7 +6,7 @@
 {
     "name": "Delivery No Weight Unit",
     "version": "10.0.1.0.0",
-    "description": """Delivery No Weight Unit""",
+    "summary": """Delivery No Weight Unit""",
     "author": "Eficent",
     "website": "www.eficent.com",
     "license": "AGPL-3",
