ef779b6d65e0436d282d891a46ca7ba2365604b5
aheficent <ahenriquez@eficent.com>
2018-07-05 16:37:09 +0200

    [ADD]analytic_category

---
+++ analytic_category/models/analytic.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class AccountAnalyticAccount(models.Model):
+    _inherit = "account.analytic.account"
+
+    category_id = fields.Many2one(
+        "account.analytic.category", "Category", ondelete="restrict")
