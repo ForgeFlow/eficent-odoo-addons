ca7742b90579d421b69a623423a4ed8a90a6b916
aheficent <ahenriquez@eficent.com>
2018-07-06 18:25:49 +0200

    [ADD]pmis

---
+++ stock_analytic_account/model/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright 2014-17 Eficent Business and IT Consulting Services S.L.
+# Copyright 2016 Matmoz d.o.o.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from . import analytic_account
+from . import stock_move
+from . import stock_picking
+from . import stock_quant
+from . import product
