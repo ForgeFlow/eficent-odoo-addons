a6574935da9499667a5cdc9f271487047d5d165a
aheficent <ahenriquez@eficent.com>
2018-07-06 18:25:49 +0200

    [ADD]pmis

---
+++ project_progress_measurement/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright 2014-17 Eficent Business and IT Consulting Services S.L.
+#        <contact@eficent.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from . import project_progress_measurement
+from . import project_project
