9b151d5f20923fd87fc07c5c2fe95442286ccf12
aheficent <ahenriquez@eficent.com>
2018-07-06 18:25:49 +0200

    [ADD]pmis

---
+++ purchase_stock_analytic/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+#  - Jordi Ballester Alomar
+# Copyright 2017 MATMOZ d.o.o.
+#  - Matjaž Mozetič
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Purchase Stock Analytic",
+    "version": "10.0.1.0.0",
+    "author": "Eficent, Odoo Community Association (OCA),"
+              "Project Expert Team",
+    "website": "http://project.expert",
+    "category": "Project Management",
+    "license": "AGPL-3",
+    "depends": [
+        "purchase",
+        "stock_analytic"
+    ],
+    "installable": True,
+}
