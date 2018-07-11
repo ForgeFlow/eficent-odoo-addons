fd1940fb6b601a336c7e77c72ed4f8276a373998
aheficent <ahenriquez@eficent.com>
2018-07-11 10:45:00 +0200

    [FIX]various test

--- analytic_resource_plan_stock_picking_operating_unit/model/analytic_resource_plan_line.py
+++ analytic_resource_plan_stock_picking_operating_unit/model/analytic_resource_plan_line.py
@@ -9,9 +9,9 @@ class AnalyticResourcePlanLine(models.Model):
     _inherit = 'analytic.resource.plan.line'
 
     @api.multi
-    def _prepare_picking_vals(self, warehouse, src_location):
+    def _prepare_picking_vals(self, src_location):
         res = super(AnalyticResourcePlanLine, self)._prepare_picking_vals(
-            warehouse, src_location
+            src_location
         )
         res['operating_unit_id'] = self.account_id.location_id.\
             operating_unit_id.id
