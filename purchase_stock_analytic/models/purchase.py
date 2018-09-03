6a94376e43d90ca7c0ef2ef0eef4cadfe5466eb3
aheficent <ahenriquez@eficent.com>
2018-09-03 17:53:37 +0200

    [FIX]we should be able to purchase for serveral AA

--- purchase_stock_analytic/models/purchase.py
+++ purchase_stock_analytic/models/purchase.py
@@ -12,6 +12,31 @@ class PurchaseOrderLine(models.Model):
 
     _inherit = "purchase.order.line"
 
+    @api.model
+    def _first_picking_copy_vals(self, key, lines):
+        """The data to be copied to new pickings is updated with data from the
+        grouping key.  This method is designed for extensibility, so that
+        other modules can store more data based on new keys."""
+        vals = super(PurchaseOrderLine, self)._first_picking_copy_vals(
+            key, lines)
+        for key_element in key:
+            if 'account_analytic_id' in key_element.keys():
+                vals['account_analytic_id'] = \
+                    key_element['account_analytic_id'].id
+        return vals
+
+    @api.model
+    def _get_group_keys(self, order, line, picking=False):
+        """Define the key that will be used to group. The key should be
+        defined as a tuple of dictionaries, with each element containing a
+        dictionary element with the field that you want to group by. This
+        method is designed for extensibility, so that other modules can add
+        additional keys or replace them by others."""
+        key = super(PurchaseOrderLine, self)._get_group_keys(order, line,
+                                                             picking=picking)
+        anal_id = line.account_analytic_id
+        return key + ({'account_analytic_id': anal_id},)
+
     @api.multi
     def _prepare_stock_moves(self, picking):
         res = super(PurchaseOrderLine, self)._prepare_stock_moves(picking)
