801c0fdf3cc960c543c13b8b13992db74a58cbe2
aheficent <ahenriquez@eficent.com>
2018-09-06 18:47:47 +0200

    [IMP]set back the option to deliver from RPL

--- stock_analytic_account/hooks.py
+++ stock_analytic_account/hooks.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright 2014-17 Eficent Business and IT Consulting Services S.L.
+# Copyright 2016 Matmoz d.o.o.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from odoo.api import Environment, SUPERUSER_ID
 import logging
 logger = logging.getLogger(__name__)
