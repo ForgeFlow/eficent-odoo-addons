cb76e11e6b544e9e8ef2c30727e353e79a7c040a
aheficent <ahenriquez@eficent.com>
2018-09-20 17:21:11 +0200

    [ADD]module account_bank_statement_check

---
+++ account_bank_statement_check/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Odoo SA
+# Copyright 2018 Eficent Business and IT Consulting Services S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lpgl.html).
+
+
+{
+    'name': 'Account Bank Statement Check',
+    'version': '10.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': "Eficent, "
+              "Odoo Community Association (OCA)",
+    'website': 'https://github.com/OCA/bank-payment',
+    'category': 'Banking addons',
+    'depends': [
+        'account',
+    ],
+    'installable': False,
+}
