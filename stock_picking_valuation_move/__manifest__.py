7da2ee4e8a42dba23fe49862b72779b46071de11
aheficent <ahenriquez@eficent.com>
2018-09-20 15:44:57 +0200

    [ADD]module stocj_picking_valuation_move

---
+++ stock_picking_valuation_move/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Eficent
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name': 'Picking Valuation Move',
+    'version': '10.0.0.1.0',
+    'category': 'Stock',
+    'summary': """Access to the valuation move from the picking""",
+    'author': "Eficent",
+    'website': 'http://www.eficent.com',
+    'license': 'AGPL-3',
+    'depends': ['stock_account'],
+    'data': [
+        'view/stock_view.xml',
+        ],
+    'installable': True,
+}
