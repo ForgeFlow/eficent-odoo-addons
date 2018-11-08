4c314310b902740a04b008601e0abf0f184719d6
aheficent <ahenriquez@eficent.com>
2018-11-08 18:08:56 +0100

    [FIX]hr_timesheet_sheet_import_accounts was not importing project

--- hr_timesheet_sheet_import_accounts/models/hr_timesheet_sheet.py
+++ hr_timesheet_sheet_import_accounts/models/hr_timesheet_sheet.py
@@ -3,7 +3,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from datetime import datetime
-from dateutil.relativedelta import relativedelta
 from odoo import _, api, exceptions, models
 from openerp.tools import DEFAULT_SERVER_DATE_FORMAT
 
@@ -12,11 +11,14 @@ class HrTimesheetSheet(models.Model):
     _inherit = "hr_timesheet_sheet.sheet"
 
     @api.multi
-    def prepare_timesheet(self, account_id, ga_id):
+    def prepare_timesheet(self, project_id, ga_id):
         for sheet in self:
+            aa = self.env['project.project'].browse(
+                project_id).analytic_account_id
             vals = {
                 'date': sheet.date_from,
-                'account_id': account_id,
+                'account_id': aa.id,
+                'project_id': project_id,
                 'name': '/',
                 'product_id': sheet.employee_id.product_id.id,
                 'product_uom_id':
@@ -24,48 +26,40 @@ class HrTimesheetSheet(models.Model):
                 'general_account_id': ga_id,
                 'amount': sheet.employee_id.product_id.standard_price,
                 'user_id': sheet.user_id.id,
+                'sheet_id': sheet.id,
             }
             return vals
 
     @api.model
     def get_accounts(self, anal_line_ids):
-        self.env.cr.execute("""SELECT DISTINCT L.account_id
+        self.env.cr.execute("""SELECT DISTINCT L.project_id
         FROM account_analytic_line AS L
         WHERE L.id IN %s
-        GROUP BY L.account_id""", (tuple(anal_line_ids),))
+        GROUP BY L.project_id""", (tuple(anal_line_ids),))
         return self.env.cr.dictfetchall()
 
     @api.model
-    def get_sheet_domain(self, date_from, date_from_lw, emp_id):
+    def get_sheet_domain(self, date_from, emp_id, period_type):
         return [('date_to', '<=', date_from),
-                ('date_from', '>=', date_from_lw),
-                ('employee_id', '=', emp_id)]
+                ('employee_id', '=', emp_id),
+                ('hr_period_id.type_id', '=', period_type.id)]
 
     @api.multi
     def set_previous_timesheet_ids(self):
         sheet_obj = self.env['hr_timesheet_sheet.sheet']
         for sheet in self:
             if sheet.state not in ('draft', 'new'):
-                raise exceptions.ValidationError(_('Timesheet not opened'))
+                raise exceptions.ValidationError(
+                    _('Timesheet draft or opened'))
             date_from = datetime.strptime(sheet.date_from,
                                           DEFAULT_SERVER_DATE_FORMAT)
-            user = self.env.user
-            r = user.company_id and user.company_id.timesheet_range or 'month'
-            delta = relativedelta(months=-1)
-            if r == 'month':
-                delta = relativedelta(months=-1)
-            elif r == 'week':
-                delta = relativedelta(weeks=-1)
-            elif r == 'year':
-                delta = relativedelta(years=-1)
-            date_from_lw = (date_from + delta).strftime(
-                DEFAULT_SERVER_DATE_FORMAT)
             emp_id = sheet.employee_id and sheet.employee_id.id or False
             if not emp_id:
                 return False
             sheet_domain = self.get_sheet_domain(
-                date_from, date_from_lw, emp_id)
-            lw_sheet_ids = sheet_obj.search(sheet_domain)
+                date_from, emp_id, sheet.hr_period_id.type_id)
+            lw_sheet_ids = sheet_obj.search(
+                sheet_domain, limit=1, order='date_to desc')
             a_line_ids = lw_sheet_ids.mapped('timesheet_ids').ids
             ga_id = sheet.employee_id.product_id.\
                 property_account_expense_id.id or \
@@ -76,7 +70,8 @@ class HrTimesheetSheet(models.Model):
                     'Please set a general expense '
                     'account in your employee view'))
             if a_line_ids:
-                accounts = self.get_accounts(a_line_ids)
-                for account_id in [a['account_id'] for a in accounts]:
-                    vals = self.prepare_timesheet(account_id, ga_id)
-                    sheet.write({'timesheet_ids': [(0, 0, vals)]})
+                projects = self.get_accounts(a_line_ids)
+                for project_id in [a['project_id'] for a in projects]:
+                    vals = sheet.prepare_timesheet(project_id, ga_id)
+                    self.env['account.analytic.line'].create(vals)
+        return True
