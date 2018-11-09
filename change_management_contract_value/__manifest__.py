9dd6bc4df42a41ac5dc2d4638c9af55e42c70bf1
aheficent <ahenriquez@eficent.com>
2018-11-09 12:23:25 +0100

    [MIG]new change_management_contract_value to v10 . Based on planning lines.

---
+++ change_management_contract_value/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# © 2015 Eficent Business and IT Consulting Services S.L. <contact@eficent.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Change Management Contract Value',
+    'version': '10.0.1.0.0',
+    'author':   'Eficent Business and IT Consulting Services S.L.',
+    'website': 'www.eficent.com',
+    'category': 'Project Management',
+    'license': 'AGPL-3',
+    'summary': 'Creates a project for each Change, child of the project '
+               'indicated.',
+    'depends': ['change_management_own_project', 'analytic_contract_value',
+                'analytic_plan'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/change_management_view.xml',
+        'views/change_management_template_view.xml',
+        'views/analytic_line_plan_view.xml',
+    ],
+    'installable': True,
+}
