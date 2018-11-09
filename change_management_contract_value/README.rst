de5f11573da09a20536fe4a74d5271ccec99acbf
aheficent <ahenriquez@eficent.com>
2018-11-09 12:23:25 +0100

    [MIG]new change_management_contract_value to v10 . Based on planning lines.

---
+++ change_management_contract_value/README.rst
@@ -0,0 +1,17 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+    :alt: License AGPL-3
+
+===============================
+Change Management Analytic plan
+===============================
+
+When confirming a change order it will create analytic_plan_lines
+
+Credits
+=======
+
+Contributors
+------------
+
+* Jordi Ballester <jordi.ballester@eficent.com>
+* Aaron Henriquez <ahenriquez@eficent.com>
---
+++ change_management_contract_value/tests/test_changes.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+# © 2015 Eficent Business and IT Consulting Services S.L. <contact@eficent.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo.tests import common
+
+
+class TestChanges(common.TransactionCase):
+
+    def setUp(self):
+
+        """***setup change tests***"""
+        super(TestChanges, self).setUp()
+        self.change_model = self.env['change.management.change']
+        self.change_template_model = self.env['change.management.template']
+        self.version_model = self.env['account.analytic.plan.version']
+        self.project_model = self.env['project.project']
+        self.product_model = self.env['product.product']
+        self.anal_model = self.env['account.analytic.account']
+        self.task_model = self.env['project.task']
+        self.user_model = self.env['res.users']
+
+        self.test_project_id = self.project_model.create(
+            {'name': 'ChangeTestProject'})
+        self.anal = self.test_project_id.analytic_account_id
+        self.product_id = self.ref('product.product_product_4')
+        self.product.product_tmpl_id.revenue_analytic_plan_journal_id = \
+            self.ref('analytic_plan.analytic_plan_journal_sale')
+        self.version_id = self.version_model.create(
+            {'name': 'name',
+             'company_id': 1,
+             'default_plan': self.ref(
+                 'analytic_plan.analytic_plan_version_P02'),
+             'active': True})
+        self.template_id = self.change_template_model.create(
+            {'name': 'name',
+             'revenue_product_id': self.product_id,
+             'version_id': self.version_id})
+        self.test_change_id = self.change_model.create({
+            'name': 'ChangeTest0001',
+            'description': 'TestChange_SkyPaintBlue',
+            'change_category_id': 1,
+            'project_id': self.test_project_id,
+            'change_value': '123.00',
+            'change_template_id': self.template_id,
+            'change_project_id': self.test_project_id})
+
+    def test_change_owner_and_creator_added_to_followers_for_change(self):
+        change = self.test_change_id
+        change.set_state_active()
+        change.set_state_accepted()
+        self.assertEqual(
+            self.anal.contract_value, 123.00, msg='wrong contract value'
+        )
+        change.set_state_draft()
+        self.assertEqual(
+            self.anal.contract_value, 00.00, msg='plan lines should be deleted'
+        )
