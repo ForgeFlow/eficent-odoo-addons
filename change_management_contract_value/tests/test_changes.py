cac8bf53b6b9e9a3b1119cbd9fcebbecfae00611
aheficent <ahenriquez@eficent.com>
2018-11-09 15:28:39 +0100

    [UPT]Add migration scripts change management v10

--- change_management_contract_value/tests/test_changes.py
+++ change_management_contract_value/tests/test_changes.py
@@ -19,30 +19,34 @@ class TestChanges(common.TransactionCase):
         self.task_model = self.env['project.task']
         self.user_model = self.env['res.users']
 
-        self.test_project_id = self.project_model.create(
+        self.test_project = self.project_model.create(
             {'name': 'ChangeTestProject'})
-        self.anal = self.test_project_id.analytic_account_id
+        self.anal = self.test_project.analytic_account_id
         self.product_id = self.ref('product.product_product_4')
+        self.product = self.product_model.browse(self.product_id)
         self.product.product_tmpl_id.revenue_analytic_plan_journal_id = \
             self.ref('analytic_plan.analytic_plan_journal_sale')
-        self.version_id = self.version_model.create(
-            {'name': 'name',
-             'company_id': 1,
-             'default_plan': self.ref(
-                 'analytic_plan.analytic_plan_version_P02'),
-             'active': True})
-        self.template_id = self.change_template_model.create(
+        self.version = self.version_model.search(
+            [('default_plan', '=', True)])
+        if not len(self.version):
+            self.version = self.version_model.create(
+                {'name': 'name',
+                 'company_id': 1,
+                 'default_plan': self.ref(
+                     'analytic_plan.analytic_plan_version_P02'),
+                 'active': True})
+        self.template = self.change_template_model.create(
             {'name': 'name',
              'revenue_product_id': self.product_id,
-             'version_id': self.version_id})
+             'version_id': self.version.id})
         self.test_change_id = self.change_model.create({
             'name': 'ChangeTest0001',
             'description': 'TestChange_SkyPaintBlue',
             'change_category_id': 1,
-            'project_id': self.test_project_id,
+            'project_id': self.test_project.id,
             'change_value': '123.00',
-            'change_template_id': self.template_id,
-            'change_project_id': self.test_project_id})
+            'change_template_id': self.template.id,
+            'change_project_id': self.test_project.id})
 
     def test_change_owner_and_creator_added_to_followers_for_change(self):
         change = self.test_change_id
