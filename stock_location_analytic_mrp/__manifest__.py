85fe5145568d1722290fd2c3897b204d04423d21
aheficent <ahenriquez@eficent.com>
2018-11-21 15:25:58 +0100

    [MIG]stock_location_analytic_mrp to v10

---
+++ stock_location_analytic_mrp/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# © 2017 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Stock Location Analytic MRP',
+    "version": "10.0.1.0.0",
+    "author": "Eficent",
+    "summary": "MRP analytic location with analytic locations",
+    "license": "AGPL-3",
+    'website': 'http://www.eficent.com',
+    'depends': [
+        'stock_location_analytic',
+        'mrp_analytic',
+    ],
+    'installable': True,
+}
