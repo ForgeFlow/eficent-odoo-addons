85ec6e8d57a0af9e3dc08f6c506c6494378ebabb
aheficent <ahenriquez@eficent.com>
2018-11-23 15:55:10 +0100

    [MIG]purchase_request_sale to v10

---
+++ purchase_request_sale/models/purchase_request.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import _, api, exceptions, fields, models
+
+
+class PurchaseRequest(models.Model):
+
+    _inherit = "purchase.request"
+
+    sale_order_id = fields.Many2one(
+        'sale.order', 'Sales Order', readonly=True)
+
+
+class PurchaseRequestLine(models.Model):
+
+    _inherit = "purchase.request.line"
+
+    sale_order_line_id = fields.Many2one(
+        'sale.order.line', 'Sales Order Line', readonly=True)
