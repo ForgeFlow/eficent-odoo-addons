40d9ef2b11d384192e2003dd8f5d3a320f5560c2
aheficent <ahenriquez@eficent.com>
2018-11-26 12:29:01 +0100

    [FIX]procurement analytic field is different

--- stock_location_analytic_procurement/models/procurement_order.py
+++ stock_location_analytic_procurement/models/procurement_order.py
@@ -15,8 +15,8 @@ class ProcurementOrder(models.Model):
         res = super(ProcurementOrder, self)._search_suitable_rule(domain)
         res_filtered = res.filtered(
             lambda rule: rule.picking_type_id.default_location_src_id.
-            analytic_account_id == self.analytic_account_id or
-            self.location_id.analytic_account_id == self.analytic_account_id)
+            analytic_account_id == self.account_analytic_id or
+            self.location_id.analytic_account_id == self.account_analytic_id)
         if res and not res_filtered:
             self.message_post(body=_('''Either the procurement location
                 does not belong to the analytic account or no destination
