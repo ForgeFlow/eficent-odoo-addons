c7bf35c0452ebe16c10734ab5e9ad39fcf759900
Aaron Henriquez <ahenriquez@eficent.com>
2019-01-30 15:10:24 +0100

    [MIG]analytic_journal v12

--- analytic_journal/__manifest__.py
+++ analytic_journal/__manifest__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 # Copyright 2015 Odoo SA
-# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2019 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
     "name": "Analytic Journal",
-    "version": "10.0.1.0.0",
+    "version": "12.0.1.0.0",
     "summary": "Analytic Journals as in previous Odoo versions",
     "author": "Odoo SA",
     "license": "LGPL-3",
--- analytic_journal/model/analytic.py
+++ analytic_journal/model/analytic.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 Odoo SA
-# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2019 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lpgl.html).
 
 from odoo import _, api, fields, models
