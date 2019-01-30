5a1e0ce36b345f2139b060c45e10b1f5cff085c1
Aaron Henriquez <ahenriquez@eficent.com>
2019-01-30 16:23:38 +0100

    [MIG]analytic_location v12

--- analytic_location/__manifest__.py
+++ analytic_location/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright 2014-17 Eficent Business and IT Consulting Services S.L.
 # Copyright 2016 Matmoz d.o.o.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Stock Location in Analytic Account',
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'author': 'Eficent, '
               'Project Expert Team, '
               'Odoo Community Association (OCA)',
@@ -19,6 +18,4 @@
     'data': [
         'view/analytic_account_view.xml',
     ],
-    'installable': True,
-    'application': True,
 }
