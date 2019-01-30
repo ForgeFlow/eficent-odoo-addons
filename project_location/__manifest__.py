da4840e7c97a21971da7f5237d0bbbdc232c2d3a
Aaron Henriquez <ahenriquez@eficent.com>
2019-01-30 17:14:30 +0100

    [MIG]project_location to v12

--- project_location/__manifest__.py
+++ project_location/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # © 2017 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Stock Location in Project",
-    "version": "10.0.1.0.0",
+    "version": "12.0.1.0.0",
     "author": "Eficent, Odoo Community Association (OCA)",
     "license": 'AGPL-3',
     "website": "www.eficent.com",
