a293d38ce2dff25e79986d3b25d96972fe5b642c
Beñat Jimenez <benat.jimenez@eficent.com>
2019-03-20 11:47:48 +0100

    [FIX] account_analytic_post_by_type: Remove the compute function and valid_to_post field

--- account_analytic_post_by_type/models/account_move.py
+++ account_analytic_post_by_type/models/account_move.py
@@ -2,32 +2,20 @@
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models
+from odoo import api, models
 
 
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
 
     @api.multi
-    def _compute_valid_to_post(self):
-        valid_list = ['Income', 'Expense', 'Cost', 'Revenue']
-        for rec in self:
-            if rec.analytic_account_id:
-                for valid in valid_list:
-                    if valid in rec.account_id.user_type_id.name:
-                        rec.valid_to_post = True
-                        return
-            rec.valid_to_post = False
-
-    valid_to_post = fields.Boolean(
-        'Entry allowed to be post',
-        compute='_compute_valid_to_post'
-    )
-
-    @api.multi
     def create_analytic_lines(self):
         new_lines = self.env['account.move.line']
+        valid_list = ['Income', 'Expense', 'Cost', 'Revenue']
         for line in self:
-            if line.valid_to_post:
-                new_lines += line
+            for valid in valid_list:
+                if (line.account_id and
+                        valid in line.account_id.user_type_id.name):
+                    new_lines += line
+                    break
         return super(AccountMoveLine, new_lines).create_analytic_lines()
