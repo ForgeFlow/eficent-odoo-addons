1a48e4f01e08b2be3da71151dfce7540e35b1d4b
Beñat Jimenez <benat.jimenez@eficent.com>
2019-05-07 13:17:31 +0200

    [12.0][MIG] product_manufacturer_partner: Migration to v12

--- product_manufacturer_partner/__init__.py
+++ product_manufacturer_partner/__init__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
-# © 2015-17 Eficent Business and IT Consulting Services S.L.
+# © 2015-19 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 from . import model
