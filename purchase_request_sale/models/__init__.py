b7ca3dfdfed1a4ebcff26773e2a705e45014a864
Beñat Jimenez <benat.jimenez@eficent.com>
2019-05-13 08:31:35 +0200

    [12.0][MIG] purchase_request_sale: Migration to v12

--- purchase_request_sale/models/__init__.py
+++ purchase_request_sale/models/__init__.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
-# Copyright 2015Eficent Business and IT Consulting Services S.L.
+# Copyright 2015-19 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from . import procurement
 from . import purchase_request
 from . import sale
+from . import stock_rule
--- purchase_request_sale/models/sale.py
+++ purchase_request_sale/models/sale.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-# Copyright 2015 Eficent Business and IT Consulting Services S.L.
+# Copyright 2015-19 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import api, fields, models, _
