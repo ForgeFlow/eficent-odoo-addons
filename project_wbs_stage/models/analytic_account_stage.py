c0b31a6982834c763a9e66ac39ea8ea2782e1f59
Beñat Jimenez <benat.jimenez@eficent.com>
2019-05-20 10:34:15 +0200

    [12.0][MIG] project_wbs_stage: Migration to v12

--- project_wbs_stage/models/analytic_account_stage.py
+++ project_wbs_stage/models/analytic_account_stage.py
@@ -1,8 +1,7 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 Eficent Business and IT Consulting Services S.L.
-# Copyright 2017 Luxim d.o.o.
-# Copyright 2017 Matmoz d.o.o.
-# Copyright 2017 Deneroteam.
+# Copyright 2017-19 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017-19 Luxim d.o.o.
+# Copyright 2017-19 Matmoz d.o.o.
+# Copyright 2017-19 Deneroteam.
 
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
