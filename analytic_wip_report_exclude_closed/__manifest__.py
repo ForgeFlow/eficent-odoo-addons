0d3db6445bdbc408cc7e403a3155d4c3276940a6
Aaron Henriquez <ahenriquez@eficent.com>
2019-06-11 12:50:19 +0200

    [IMP]WIP statement show closed only

--- analytic_wip_report_exclude_closed/__manifest__.py
+++ analytic_wip_report_exclude_closed/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Work In Progress Statement Exclude Closed Projects",
-    "version": "10.0.1.0.0",
+    "version": "10.0.2.0.0",
     "author": "Eficent Business and IT Consulting Services S.L.,"
               "Odoo Community Association (OCA)",
     "summary": """Filter closed projects in analytic wip report""",
