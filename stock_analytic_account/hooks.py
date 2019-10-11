608b1e72905549ebc0768df278ec8b551aaf90eb
ahenriquez <ahenriquez@eficent.com>
2019-10-11 10:15:14 +0200

    [FIX]stock_analytic_Account pre init hook minor fix

--- stock_analytic_account/hooks.py
+++ stock_analytic_account/hooks.py
@@ -9,7 +9,7 @@ logger = logging.getLogger(__name__)
 def pre_init_hook(cr):
     env = Environment(cr, SUPERUSER_ID, {})
     # creating missing anaytic journals if they are not created
-    for aa in env['account.journal'].search(
+    for aa in env['account.journal'].with_context(active_test=False).search(
             [('analytic_journal_id', '=', False)]):
         aa.analytic_journal_id = env['account.analytic.journal'].create(
             {'type': aa.type,
