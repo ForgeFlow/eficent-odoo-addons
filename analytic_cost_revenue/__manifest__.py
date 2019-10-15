a0e8521f87cbc4ac976e83954157a48a942ab87d
ahenriquez <ahenriquez@eficent.com>
2019-10-15 18:19:23 +0200

    [MIG]analytic_cost_revenue to v12

--- analytic_cost_revenue/__manifest__.py
+++ analytic_cost_revenue/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Analytic Cost and Revenue",
-    "version": "10.0.1.0.0",
+    "version": "12.0.1.0.0",
     "author": "Eficent",
     "category": "Projects",
     "Summary": "Analytic Cost and Revenue to analytic accounts ",
@@ -16,8 +15,6 @@
         "account_analytic_parent",
         "analytic_journal",
     ],
-    "data": [
-        "views/analytic_view.xml"
-    ],
+    "data": ["views/analytic_view.xml"],
     "installable": True,
 }
