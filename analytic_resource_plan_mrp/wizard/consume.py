df4c33c90db291e605a639c4a1987e2953e5f576
ahenriquez <ahenriquez@eficent.com>
2019-10-15 13:23:37 +0200

    [MIG]analytic_resource_plan_mrp to v12

--- analytic_resource_plan_mrp/wizard/consume.py
+++ analytic_resource_plan_mrp/wizard/consume.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015-17 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
@@ -11,52 +10,52 @@ class AnalyticResourcePlanLineConsume(models.TransientModel):
     _description = "Consume from Resource Plan Lines"
 
     item_ids = fields.One2many(
-        'analytic.resource.plan.line.consume.item',
-        'wiz_id',
-        'Items'
+        "analytic.resource.plan.line.consume.item", "wiz_id", "Items"
     )
 
     def _prepare_item(self, line):
         return {
-            'product_qty': line.unit_amount,
-            'product_uom_id': line.product_uom_id.id,
-            'line_id': line.id,
+            "product_qty": line.unit_amount,
+            "product_uom_id": line.product_uom_id.id,
+            "line_id": line.id,
         }
 
     @api.model
     def default_get(self, fields):
         res = super(AnalyticResourcePlanLineConsume, self).default_get(fields)
-        res_plan_obj = self.env['analytic.resource.plan.line']
-        resource_plan_line_ids = self._context.get('active_ids', [])
-        active_model = self._context.get('active_model')
+        res_plan_obj = self.env["analytic.resource.plan.line"]
+        resource_plan_line_ids = self._context.get("active_ids", [])
+        active_model = self._context.get("active_model")
 
         if not resource_plan_line_ids:
             return res
-        assert active_model == 'analytic.resource.plan.line', \
-            'Bad context propagation'
+        assert (
+            active_model == "analytic.resource.plan.line"
+        ), "Bad context propagation"
 
         items = []
         for line in res_plan_obj.browse(resource_plan_line_ids):
             items.append((0, 0, self._prepare_item(line)))
-        res.update({'item_ids': items})
+        res.update({"item_ids": items})
         return res
 
     def do_consume(self):
         res = []
-        res_plan_obj = self.env['analytic.resource.plan.line']
+        res_plan_obj = self.env["analytic.resource.plan.line"]
         for item in self.item_ids:
-            move_id = res_plan_obj.create_consume_move(item.line_id.id,
-                                                       item.product_qty)
+            move_id = res_plan_obj.create_consume_move(
+                item.line_id.id, item.product_qty
+            )
             res.append(move_id.id)
         return {
-            'domain': "[('id','in', ["+','.join(map(str, res))+"])]",
-            'name': _('Stock Moves'),
-            'view_type': 'form',
-            'view_mode': 'tree,form',
-            'res_model': 'stock.move',
-            'view_id': False,
-            'context': False,
-            'type': 'ir.actions.act_window'
+            "domain": "[('id','in', [" + ",".join(map(str, res)) + "])]",
+            "name": _("Stock Moves"),
+            "view_type": "form",
+            "view_mode": "tree,form",
+            "res_model": "stock.move",
+            "view_id": False,
+            "context": False,
+            "type": "ir.actions.act_window",
         }
 
 
@@ -65,25 +64,24 @@ class AnalyticResourcePlanLineConsumeItem(models.TransientModel):
     _description = "Resource plan consume item"
 
     wiz_id = fields.Many2one(
-        'analytic.resource.plan.line.consume',
-        'Wizard',
+        "analytic.resource.plan.line.consume",
+        "Wizard",
         required=True,
-        ondelete='cascade',
-        readonly=True
+        ondelete="cascade",
+        readonly=True,
     )
     line_id = fields.Many2one(
-        'analytic.resource.plan.line',
-        'Resource Plan Line',
+        "analytic.resource.plan.line",
+        "Resource Plan Line",
         required=True,
-        readonly=True
+        readonly=True,
     )
     product_qty = fields.Float(
-        string='Quantity to consume',
-        digits=dp.get_precision('Product UoS')
+        string="Quantity to consume", digits=dp.get_precision("Product UoS")
     )
     product_uom_id = fields.Many2one(
-        'product.uom',
-        related='line_id.product_uom_id',
-        string='UoM',
-        readonly=True
+        "uom.uom",
+        related="line_id.product_uom_id",
+        string="UoM",
+        readonly=True,
     )
