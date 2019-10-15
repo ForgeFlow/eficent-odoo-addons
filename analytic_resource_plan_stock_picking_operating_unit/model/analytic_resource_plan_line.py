7e61c44bf2406793306460fad363b46b287d943b
ahenriquez <ahenriquez@eficent.com>
2019-10-15 12:52:20 +0200

    [MIG]analytic_resource_plan_stock_picking_operating_unit to v12

--- analytic_resource_plan_stock_picking_operating_unit/model/analytic_resource_plan_line.py
+++ analytic_resource_plan_stock_picking_operating_unit/model/analytic_resource_plan_line.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from odoo import api, models
@@ -6,13 +5,14 @@ from odoo import api, models
 
 class AnalyticResourcePlanLine(models.Model):
 
-    _inherit = 'analytic.resource.plan.line'
+    _inherit = "analytic.resource.plan.line"
 
     @api.multi
     def _prepare_picking_vals(self, src_location):
         res = super(AnalyticResourcePlanLine, self)._prepare_picking_vals(
             src_location
         )
-        res['operating_unit_id'] = self.account_id.location_id.\
-            operating_unit_id.id
+        res[
+            "operating_unit_id"
+        ] = self.account_id.location_id.operating_unit_id.id
         return res
