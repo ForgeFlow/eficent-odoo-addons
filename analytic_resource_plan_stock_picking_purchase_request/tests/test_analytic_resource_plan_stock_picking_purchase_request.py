b7623c3eb11b66e85893b29f253116a9e723d10f
ahenriquez <ahenriquez@eficent.com>
2019-10-16 14:45:17 +0200

    [FIX]test refactor to make travis happy

--- analytic_resource_plan_stock_picking_purchase_request/tests/test_analytic_resource_plan_stock_picking_purchase_request.py
+++ analytic_resource_plan_stock_picking_purchase_request/tests/test_analytic_resource_plan_stock_picking_purchase_request.py
@@ -1,16 +1,46 @@
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo.addons.analytic_resource_plan_stock_picking.tests import (
-    test_analytic_resource_plan_stock_picking,
+from odoo.addons.analytic_resource_plan_stock.tests import (
+    test_analytic_resource_plan_stock,
 )
 
 
 class TestAnalyticResourcePlanStockPickingPurchaseRequest(
-    test_analytic_resource_plan_stock_picking.
-        TestAnalyticResourcePlanStockPicking):
+    test_analytic_resource_plan_stock.TestAnalyticResourcePlanStock
+):
     def setUp(cls):
-        super(TestAnalyticResourcePlanStockPickingPurchaseRequest, cls).setUp()
+        super(
+            TestAnalyticResourcePlanStockPickingPurchaseRequest, cls).setUp()
+        cls.analytic_account_obj = cls.env["account.analytic.account"]
+        cls.resource_plan_line_obj = cls.env["analytic.resource.plan.line"]
+        cls.product_id = cls.env.ref("product.product_product_27")
+        cls.partner = cls.env.ref("base.res_partner_1")
+        cls.analytic_plan_version = cls.env.ref(
+            "analytic_plan.analytic_plan_version_P00"
+        )
+        cls.location_id = cls.env.ref("stock.stock_location_stock")
+        cls.product_id.write({"expense_analytic_plan_journal_id": 1})
+        cls.analytic_plan_journal_obj = cls.env[
+            "account.analytic.plan.journal"
+        ]
+        cls.purchase_request_obj = cls.env["purchase.request"]
+        cls.purchase_request_line_obj = cls.env["purchase.request.line"]
+        cls.partner = cls.env.ref("base.res_partner_1")
+        cls.analytic_plan_version = cls.env.ref(
+            "analytic_plan.analytic_plan_version_P00"
+        )
+        cls.product_id = cls.env.ref("product.product_product_27")
+        cls.location_id = cls.env.ref("stock.stock_location_stock")
+        cls.analytic_plan_journal = cls.analytic_plan_journal_obj.create(
+            {"name": "Sale", "type": "sale", "code": "SAL", "active": True}
+        )
+        cls.product_id.write(
+            {
+                "expense_analytic_plan_journal_id":
+                    cls.analytic_plan_journal.id
+            }
+        )
 
     def test_analytic_resource_plan_stock_picking(cls):
         cls.resource_plan_line.unit_amount = 2.0
