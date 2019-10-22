1c58ec8d4e33bbff699b14b4c1a845bc25cd3f65
ahenriquez <ahenriquez@eficent.com>
2019-10-22 15:18:20 +0200

    [MIG]analytic_wip_report_exclude_closed to v12

--- analytic_wip_report_exclude_closed/__manifest__.py
+++ analytic_wip_report_exclude_closed/__manifest__.py
@@ -1,12 +1,11 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Work In Progress Statement Exclude Closed Projects",
-    "version": "10.0.2.0.0",
+    "version": "12.0.2.0.0",
     "author": "Eficent Business and IT Consulting Services S.L.,"
-              "Odoo Community Association (OCA)",
+    "Odoo Community Association (OCA)",
     "summary": """Filter closed projects in analytic wip report""",
     "category": "Generic Modules/Projects & Services",
     "license": "AGPL-3",
@@ -16,8 +15,6 @@
         "analytic_schedule",
         "analytic_wip_report_fiscalyear",
     ],
-    "data": [
-        "wizards/analytic_wip_report_view.xml"
-    ],
+    "data": ["wizards/analytic_wip_report_view.xml"],
     "installable": True,
 }
