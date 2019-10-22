43233bc9fc3fbef139ecc60732decc25760aea42
ahenriquez <ahenriquez@eficent.com>
2019-10-22 17:32:33 +0200

    [MIG]project_stage_use_timesheets to v12

--- project_stage_use_timesheets/models/base_kanban_stage.py
+++ project_stage_use_timesheets/models/base_kanban_stage.py
@@ -1,14 +1,15 @@
-# -*- coding: utf-8 -*-
 # © 2015-17 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
 
 
-class AnalyticAccountStage(models.Model):
-    _inherit = 'analytic.account.stage'
+class BaseKanbanStage(models.Model):
+    _inherit = "base.kanban.stage"
 
     allow_timesheets = fields.Boolean(
-        'Timesheets', default=True,
+        "Timesheets",
+        default=True,
         help="Check this field if setting this stage should make the project"
-             " to manage timesheets")
+        " to manage timesheets",
+    )
--- project_stage_use_timesheets/models/project_project.py
+++ project_stage_use_timesheets/models/project_project.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # © 2015-17 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
@@ -6,17 +5,16 @@ from odoo import api, models
 
 
 class ProjectProject(models.Model):
-    _inherit = 'project.project'
+    _inherit = "project.project"
 
+    @api.depends("stage_id", "stage_id.allow_timesheets")
     @api.multi
-    def write(self, values):
-        for project in self:
-            if values.get('stage_id', False):
-                stage_obj = self.env['analytic.account.stage']
-                stage = stage_obj.browse(values['stage_id'])
-                if stage.allow_timesheets and project.account_class == \
-                        'work_package':
-                    values['allow_timesheets'] = True
-                elif not stage.allow_timesheets:
-                    values['allow_timesheets'] = False
-        return super(ProjectProject, self).write(values)
+    def check_use_timesheets(self):
+        for pp in self:
+            if (
+                pp.stage_id.allow_timesheets
+                and pp.analytic_account_id.account_class == "work_package"
+            ):
+                self.allow_timesheets = True
+            elif not pp.stage_id.allow_timesheets:
+                self.allow_timesheets = False
