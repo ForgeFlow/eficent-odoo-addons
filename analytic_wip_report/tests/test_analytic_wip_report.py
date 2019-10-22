f678ccd8973db78d988f3377dc2e24ec39b8bf5d
ahenriquez <ahenriquez@eficent.com>
2019-10-22 12:44:57 +0200

    [MIG]analytic_wip_report to v12

--- analytic_wip_report/tests/test_analytic_wip_report.py
+++ analytic_wip_report/tests/test_analytic_wip_report.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from odoo.tests import common
@@ -7,33 +6,47 @@ from odoo.tests import common
 class TestAnalyticWipReport(common.TransactionCase):
     def setUp(self):
         super(TestAnalyticWipReport, self).setUp()
-        self.AnalyticAccountObject = self.env['account.analytic.account']
-        self.account_invoice = self.env['account.invoice']
-        self.account_model = self.env['account.account']
+        self.AnalyticAccountObject = self.env["account.analytic.account"]
+        self.account_invoice = self.env["account.invoice"]
+        self.account_model = self.env["account.account"]
 
-        self.partner = self.env.ref('base.res_partner_2')
-        self.receivable = self.env.ref('account.data_account_type_receivable')
-        self.analytic_plan_version =\
-            self.env.ref('analytic_plan.analytic_plan_version_P02')
+        self.partner = self.env.ref("base.res_partner_2")
+        self.receivable = self.env.ref("account.data_account_type_receivable")
+        self.analytic_plan_version = self.env.ref(
+            "analytic_plan.analytic_plan_version_P02"
+        )
 
-        self.account = self.AnalyticAccountObject.create({
-            'name': 'AnalyticAccount Parent for Test',
-            'partner_id': self.partner.id
-        })
+        self.account = self.AnalyticAccountObject.create(
+            {
+                "name": "AnalyticAccount Parent for Test",
+                "partner_id": self.partner.id,
+            }
+        )
 
     def test_check_wip_report(self):
         self.invoice_account = self.account_model.search(
-            [('user_type_id', '=', self.receivable.id)], limit=1)
-        self.invoice = self.account_invoice.create({
-            'partner_id': self.partner.id,
-            'type': 'out_invoice',
-            'account_id': self.invoice_account.id,
-            'invoice_line_ids': [(0, 0, {
-                'name': 'Test invoice line',
-                'account_id': self.invoice_account.id,
-                'quantity': 2,
-                'price_unit': 100,
-                'account_analytic_id': self.account.id})]})
+            [("user_type_id", "=", self.receivable.id)], limit=1
+        )
+        self.invoice = self.account_invoice.create(
+            {
+                "partner_id": self.partner.id,
+                "type": "out_invoice",
+                "account_id": self.invoice_account.id,
+                "invoice_line_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "name": "Test invoice line",
+                            "account_id": self.invoice_account.id,
+                            "quantity": 2,
+                            "price_unit": 100,
+                            "account_analytic_id": self.account.id,
+                        },
+                    )
+                ],
+            }
+        )
         self.invoice.action_invoice_open()
         self.account._compute_wip_report()
         self.assertEquals(self.account.earned_revenue, 0)
