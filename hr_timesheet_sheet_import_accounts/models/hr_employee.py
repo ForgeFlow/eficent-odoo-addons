71352f3d4c76b77921786ebaa166ce1817f8df0e
ahenriquez <ahenriquez@eficent.com>
2019-10-23 12:18:43 +0200

    [MIG]hr_timesheet_sheet_import_accounts to v12

--- hr_timesheet_sheet_import_accounts/models/hr_employee.py
+++ hr_timesheet_sheet_import_accounts/models/hr_employee.py
@@ -1,6 +1,3 @@
-# -*- coding: utf-8 -*-
-# Copyright 2014-17 Eficent Business and IT Consulting Services, S.L.
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from odoo import api, exceptions, fields, models, _
 
 
@@ -8,12 +5,14 @@ class HrEmployee(models.Model):
     _inherit = "hr.employee"
 
     product_id = fields.Many2one(
-        comodel_name='product.product', string='Product', required=True)
+        comodel_name="product.product", string="Product"
+    )
 
     @api.multi
-    @api.constrains('product_id')
+    @api.constrains("product_id")
     def _check_is_employee(self):
         for rec in self:
             if not rec.product_id.is_employee:
-                raise exceptions.ValidationError(_(
-                    "The product is not marked as employee"))
+                raise exceptions.ValidationError(
+                    _("The product is not marked as employee")
+                )
--- hr_timesheet_sheet_import_accounts/models/hr_timesheet_sheet.py
+++ hr_timesheet_sheet_import_accounts/models/hr_timesheet_sheet.py
@@ -1,77 +1,88 @@
-# -*- coding: utf-8 -*-
-# Copyright 2014-17 Eficent Business and IT Consulting Services, S.L.
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
 from datetime import datetime
 from odoo import _, api, exceptions, models
 from openerp.tools import DEFAULT_SERVER_DATE_FORMAT
 
 
 class HrTimesheetSheet(models.Model):
-    _inherit = "hr_timesheet_sheet.sheet"
+    _inherit = "hr_timesheet.sheet"
 
     @api.multi
-    def prepare_timesheet(self, project_id, ga_id):
+    def prepare_timesheet(self, project_id):
         for sheet in self:
-            aa = self.env['project.project'].browse(
-                project_id).analytic_account_id
+            aa = (
+                self.env["project.project"]
+                .browse(project_id)
+                .analytic_account_id
+            )
+            ga_id = self.env[
+                "account.analytic.line"
+            ]._getGeneralAccountFromCostCategory(aa, sheet.user_id)
+            if not ga_id:
+                ga_id = (
+                    sheet.employee_id.product_id.property_account_expense_id.id
+                    or sheet.employee_id.product_id.categ_id.property_account_expense_categ_id.id
+                )
+            if not ga_id:
+                raise exceptions.ValidationError(
+                    _(
+                        "Please set a general expense "
+                        "account in your employee view"
+                    )
+                )
             vals = {
-                'date': sheet.date_from,
-                'account_id': aa.id,
-                'project_id': project_id,
-                'name': '/',
-                'product_id': sheet.employee_id.product_id.id,
-                'product_uom_id':
-                    sheet.employee_id.product_id.uom_id.id,
-                'general_account_id': ga_id,
-                'amount': sheet.employee_id.product_id.standard_price,
-                'user_id': sheet.user_id.id,
-                'sheet_id': sheet.id,
+                "date": sheet.date_start,
+                "account_id": aa.id,
+                "project_id": project_id,
+                "name": "/",
+                "product_id": sheet.employee_id.product_id.id,
+                "product_uom_id": sheet.employee_id.product_id.uom_id.id,
+                "general_account_id": ga_id.id,
+                "user_id": sheet.user_id.id,
+                "sheet_id": sheet.id,
             }
             return vals
 
     @api.model
     def get_accounts(self, anal_line_ids):
-        self.env.cr.execute("""SELECT DISTINCT L.project_id
+        self.env.cr.execute(
+            """SELECT DISTINCT L.project_id
         FROM account_analytic_line AS L
         WHERE L.id IN %s
-        GROUP BY L.project_id""", (tuple(anal_line_ids),))
+        GROUP BY L.project_id""",
+            (tuple(anal_line_ids),),
+        )
         return self.env.cr.dictfetchall()
 
     @api.model
-    def get_sheet_domain(self, date_from, emp_id, period_type):
-        return [('date_to', '<=', date_from),
-                ('employee_id', '=', emp_id),
-                ('hr_period_id.type_id', '=', period_type.id)]
+    def get_sheet_domain(self, date_start, emp_id, period_type):
+        return [
+            ("date_end", "<=", date_start),
+            ("employee_id", "=", emp_id),
+            ("hr_period_id.type_id", "=", period_type.id),
+        ]
 
     @api.multi
     def set_previous_timesheet_ids(self):
-        sheet_obj = self.env['hr_timesheet_sheet.sheet']
+        sheet_obj = self.env["hr_timesheet.sheet"]
         for sheet in self:
-            if sheet.state not in ('draft', 'new'):
+            if sheet.state not in ("draft", "new"):
                 raise exceptions.ValidationError(
-                    _('Timesheet draft or opened'))
-            date_from = datetime.strptime(sheet.date_from,
-                                          DEFAULT_SERVER_DATE_FORMAT)
+                    _("Timesheet draft or opened")
+                )
+            date_start = sheet.date_start
             emp_id = sheet.employee_id and sheet.employee_id.id or False
             if not emp_id:
                 return False
             sheet_domain = self.get_sheet_domain(
-                date_from, emp_id, sheet.hr_period_id.type_id)
+                date_start, emp_id, sheet.hr_period_id.type_id
+            )
             lw_sheet_ids = sheet_obj.search(
-                sheet_domain, limit=1, order='date_to desc')
-            a_line_ids = lw_sheet_ids.mapped('timesheet_ids').ids
-            ga_id = sheet.employee_id.product_id.\
-                property_account_expense_id.id or \
-                sheet.employee_id.product_id.\
-                categ_id.property_account_expense_categ_id.id
-            if not ga_id:
-                raise exceptions.ValidationError(_(
-                    'Please set a general expense '
-                    'account in your employee view'))
+                sheet_domain, limit=1, order="date_end desc"
+            )
+            a_line_ids = lw_sheet_ids.mapped("timesheet_ids").ids
             if a_line_ids:
                 projects = self.get_accounts(a_line_ids)
-                for project_id in [a['project_id'] for a in projects]:
-                    vals = sheet.prepare_timesheet(project_id, ga_id)
-                    self.env['account.analytic.line'].create(vals)
+                for project_id in [a["project_id"] for a in projects]:
+                    vals = sheet.prepare_timesheet(project_id)
+                    self.env["account.analytic.line"].create(vals)
         return True
