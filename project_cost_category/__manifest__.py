942465cd16aeaaba5c971493cd2c4c6dde85ce7a
ahenriquez <ahenriquez@eficent.com>
2019-10-23 13:38:12 +0200

    [MIG]project_cost_category to v12

--- project_cost_category/__manifest__.py
+++ project_cost_category/__manifest__.py
@@ -1,22 +1,16 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015-17 Eficent Business and IT Consulting Services S.L.
 #   (http://www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Project Cost Category",
-    "version": "10.0.1.0.0",
+    "version": "12.0.1.0.0",
     "summary": """Adds the cost category to projects.""",
     "author": "Eficent, Odoo Community Association (OCA)",
     "website": "http://www.eficent.com",
     "category": "Generic Modules/Projects & Services",
     "license": "AGPL-3",
-    "depends": [
-        "project",
-        "account_analytic_cost_category"
-    ],
-    "data": [
-        "views/project_view.xml",
-    ],
+    "depends": ["project", "account_analytic_cost_category"],
+    "data": ["views/project_view.xml"],
     "installable": True,
 }
--- project_cost_category/models/project.py
+++ project_cost_category/models/project.py
@@ -1,19 +1,13 @@
-# -*- coding: utf-8 -*-
-# Copyright 2015-17 Eficent Business and IT Consulting Services S.L.
-#   (http://www.eficent.com)
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-
 from odoo import fields, models
 
 
 class Project(models.Model):
-    _inherit = 'project.project'
+    _inherit = "project.project"
 
     cost_category = fields.Selection(
-        [('cogs', 'Cost of Goods Sold'),
-         ('expense', 'Expense')],
-        string='Type of Cost',
+        [("cogs", "Cost of Goods Sold"), ("expense", "Expense")],
+        string="Type of Cost",
         help="""Defines what type of cost does the analytic account carry
         from an employee perspective.""",
-        default='cogs'
+        default="cogs",
     )
