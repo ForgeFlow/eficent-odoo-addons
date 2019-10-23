928ab62b0660812bffcf18b1fba8d94f653290d3
ahenriquez <ahenriquez@eficent.com>
2019-10-23 13:32:27 +0200

    [MIG]project_mrp_production to v12

--- project_mrp_production/__manifest__.py
+++ project_mrp_production/__manifest__.py
@@ -1,11 +1,11 @@
-# -*- coding: utf-8 -*-
 # © 2014-17 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Project Manufacturing Order Link",
-    "version": "10.0.1.0.0",
+    "version": "12.0.1.0.0",
     "author": "Eficent",
+    "license": "AGPL-3",
     "website": "www.eficent.com",
     "category": "Generic Modules/Projects & Services",
     "depends": ["project", "mrp_analytic"],
