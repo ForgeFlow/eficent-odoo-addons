c1ff6a248a20338328a6adb7a0a46660da1f2549
ahenriquez <ahenriquez@eficent.com>
2019-10-23 13:16:32 +0200

    [MIG]mrp_analytic_location to v12

--- mrp_analytic_location/models/mrp_production.py
+++ mrp_analytic_location/models/mrp_production.py
@@ -1,14 +1,10 @@
-# -*- coding: utf-8 -*-
-# Copyright 2018 Eficent
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-
 from odoo import _, exceptions, api, fields, models
 
 
 class MrpProduction(models.Model):
-    _inherit = 'mrp.production'
+    _inherit = "mrp.production"
 
-    @api.depends('analytic_account_id', 'location_src_id', 'location_dest_id')
+    @api.depends("analytic_account_id")
     def compute_project_location(self):
         for mnf in self:
             if mnf.analytic_account_id:
@@ -17,11 +13,16 @@ class MrpProduction(models.Model):
                     mnf.location_dest_id = mnf.analytic_account_id.location_id
                 else:
                     raise exceptions.ValidationError(
-                        _('Please create or assign  a location for the '
-                          'analytic account'))
+                        _(
+                            "Please create or assign  a location for the "
+                            "analytic account"
+                        )
+                    )
         return True
 
     location_src_id = fields.Many2one(
-        compute=compute_project_location, store=True)
+        compute=compute_project_location, store=True
+    )
     location_dest_id = fields.Many2one(
-        compute=compute_project_location, store=True)
+        compute=compute_project_location, store=True
+    )
