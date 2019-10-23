3601540a777f3520a73b8566ec6b091dd28d100c
ahenriquez <ahenriquez@eficent.com>
2019-10-23 12:34:38 +0200

    [MIG]hr_timesheet_sheet_validators to v12

--- hr_timesheet_sheet_validators/__init__.py
+++ hr_timesheet_sheet_validators/__init__.py
@@ -1,6 +1,1 @@
-# -*- coding: utf-8 -*-
-# Copyright 2014-17 Eficent Business and IT Consulting Services, S.L.
-# Copyright 2017 Serpent Consulting Services Pvt. Ltd.
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
 from . import models
--- hr_timesheet_sheet_validators/__manifest__.py
+++ hr_timesheet_sheet_validators/__manifest__.py
@@ -1,24 +1,17 @@
-# -*- coding: utf-8 -*-
-# Copyright 2014-17 Eficent Business and IT Consulting Services, S.L.
-# Copyright 2017 Serpent Consulting Services Pvt. Ltd.
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
 {
-    'name': 'HR Timesheet Sheet Validators',
-    'version': '10.0.1.0.0',
-    'author': 'Eficent, Odoo Community Association (OCA)',
-    'category': 'Human Resources',
-    'summary': """This module allows a user outside of the Human Resources
+    "name": "HR Timesheet Sheet Validators",
+    "version": "12.0.1.0.0",
+    "author": "Eficent, Odoo Community Association (OCA)",
+    "category": "Human Resources",
+    "summary": """This module allows a user outside of the Human Resources
                groups to validate timesheets. A rule is predefined, but it
                is flexible enough to accept extensions.""",
-    'website': 'http://www.eficent.com',
+    "website": "http://www.eficent.com",
     "license": "LGPL-3",
-    'depends': [
-        'hr_timesheet_sheet'
+    "depends": ["hr_timesheet_sheet"],
+    "data": [
+        "security/hr_timesheet_sheet_security.xml",
+        "views/hr_timesheet_sheet_view.xml",
     ],
-    'data': [
-        'security/hr_timesheet_sheet_security.xml',
-        'views/hr_timesheet_sheet_view.xml',
-    ],
-    'installable': True,
+    "installable": True,
 }
