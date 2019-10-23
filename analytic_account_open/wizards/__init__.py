c42a76e8ce10fda4492c34c5e3ac1f848c6b594f
ahenriquez <ahenriquez@eficent.com>
2019-10-23 16:09:19 +0200

    [MIG]analytic_account_open to v12

--- analytic_account_open/wizards/__init__.py
+++ analytic_account_open/wizards/__init__.py
@@ -1,5 +1,1 @@
-# -*- coding: utf-8 -*-
-# Copyright 2015 Eficent - Jordi Ballester Alomar
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-
 from . import analytic_account_open
