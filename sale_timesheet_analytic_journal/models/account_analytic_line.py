4adb03fc5af644740bbf95b23473382200c5ba3b
ahenriquez <ahenriquez@eficent.com>
2019-10-23 16:58:19 +0200

    [MIG]sale_timesheet_analytic_journal to v12

--- sale_timesheet_analytic_journal/models/account_analytic_line.py
+++ sale_timesheet_analytic_journal/models/account_analytic_line.py
@@ -1,21 +1,19 @@
-# -*- coding: utf-8 -*-
-# Copyright 2018 Eficent Business and IT Consulting Services S.L.
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-
-from odoo import models, _
+from odoo import _, api, models
 from odoo.exceptions import ValidationError
 
 
 class AccountAnalyticLine(models.Model):
-    _inherit = 'account.analytic.line'
+    _inherit = "account.analytic.line"
 
-    def _get_timesheet_cost(self, values):
-        res = super(AccountAnalyticLine, self)._get_timesheet_cost(values)
-        if not values.get('journal_id', False):
-            labor_anal_journal = self.env['account.analytic.journal'].search(
-                [('cost_type', '=', 'labor')], limit=1)
+    @api.constrains("timesheet_invoice_id", "journal_id")
+    def _constrains_exclude_from_sale_order(self):
+        if self.filtered(
+            lambda line: line.timesheet_invoice_id and not line.journal_id
+        ):
+            labor_anal_journal = self.env["account.analytic.journal"].search(
+                [("cost_type", "=", "labor")], limit=1
+            )
             if not labor_anal_journal:
                 raise ValidationError(
-                    _("Please create an analytic journal for labor cost"))
-            res['journal_id'] = labor_anal_journal.id
-        return res
+                    _("Please create an analytic journal for labor cost")
+                )
