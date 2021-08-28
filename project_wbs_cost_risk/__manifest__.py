88193e4560532600cb7409695ffa34c1a9319b4a
AaronHForgeFlow <ahenriquez@eficent.com>
2021-08-28 16:17:18 +0200

    [ADD] project_wbs_cost_risk

---
+++ project_wbs_cost_risk/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2021 FrgeFlow S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Project WBS Cost Risk",
+    "version": "10.0.1.0.0",
+    "summary": "Cost Risk in WBS deliverables based on timesheets",
+    "author": "ForgeFlow",
+    "license": "AGPL-3",
+    "website": "http://www.forgeflow.com",
+    "category": "Project",
+    "depends": ["project_wbs", "hr_timesheet_sheet", "web_tree_dynamic_colored_field"],
+    "data": [
+        "data/ir_cron.xml",
+        "views/account_analytic_account_view.xml",
+        "views/project_project_view.xml",        
+    ],
+    'post_init_hook': 'post_init_hook',
+    "installable": True,
+}
---
+++ project_wbs_cost_risk/tests/__init__.py
@@ -0,0 +1,1 @@
+from . import test_project_wbs_cost_risk
