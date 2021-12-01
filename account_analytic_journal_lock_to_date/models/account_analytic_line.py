0c227c7b633040234aef08a9e96f8843313ae9d3
AaronHForgeFlow <ahenriquez@eficent.com>
2021-12-01 14:19:38 +0100

    [10.0][ADD] account_analytic_journal_lock_to_date

---
+++ account_analytic_journal_lock_to_date/models/account_analytic_line.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 ForgeFlow S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, models, _
+from odoo.exceptions import ValidationError
+from datetime import timedelta
+from datetime import datetime
+from odoo.tools import DEFAULT_SERVER_DATE_FORMAT as DT
+
+
+class AccountAnalyticLine(models.Model):
+
+    _inherit = 'account.analytic.line'
+
+    @api.multi
+    @api.constrains('date')
+    def _check_lock_date(self):
+        for line in self:
+            if not line.journal_id.restrict_lock_dates:
+                return True
+            lock_date = line.journal_id.journal_lock_to_date
+            if not lock_date:
+                lock_date = datetime.now().date() + timedelta(days=1)
+            else:
+                lock_date = datetime.strptime(lock_date, DT).date()
+            if lock_date and datetime.strptime(line.date, DT).date() >= lock_date:
+                raise ValidationError(
+                    _("You cannot add/modify entries after %s" % lock_date))
+        return True
