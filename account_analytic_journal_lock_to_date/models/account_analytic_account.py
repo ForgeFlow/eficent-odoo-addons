e3445f9937d4a75995350493c88ba7aa533a8b52
AaronHForgeFlow <ahenriquez@eficent.com>
2021-12-02 12:30:38 +0100

    [10.0][IMP] Allow analytic account to surpass lock dates

---
+++ account_analytic_journal_lock_to_date/models/account_analytic_account.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 ForgeFlow S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, models, fields
+
+
+class AccountAnalyticAccount(models.Model):
+    _inherit = 'account.analytic.account'
+
+    surpass_lock_dates = fields.Boolean()
+
+    @api.multi
+    def write(self, vals):
+        # propagate surpass_lock_dates in the hierarchy
+        if self and 'surpass_lock_dates' in vals and not vals['surpass_lock_dates']:
+            self.mapped('child_ids').write({'surpass_lock_dates': False})
+        elif self and 'surpass_lock_dates' in vals and vals['surpass_lock_dates']:
+            self.mapped('child_ids').write({'surpass_lock_dates': True})
+        return super(AccountAnalyticAccount, self).write(vals)
