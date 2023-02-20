9332158a4d14662bdcffb39be86ba80ee0d1c194
JasminSForgeFlow <jasmin.solanki@forgeflow.com>
2023-02-20 10:55:17 +0530

    [IMP] analytic_journal: black, isort, prettier

--- analytic_journal/__manifest__.py
+++ analytic_journal/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 Odoo SA
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
@@ -11,7 +10,9 @@
     "license": "LGPL-3",
     "category": "Analytic",
     "depends": ["account", "analytic"],
-    "data": ['views/analytic_view.xml',
-             'security/ir.model.access.csv', ],
-    'installable': True,
+    "data": [
+        "views/analytic_view.xml",
+        "security/ir.model.access.csv",
+    ],
+    "installable": True,
 }
