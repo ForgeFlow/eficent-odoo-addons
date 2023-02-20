43e48eed7fa010dadb2b735a2aca7cc62545987a
JasminSForgeFlow <jasmin.solanki@forgeflow.com>
2023-02-20 15:44:38 +0530

    [MIG] analytic_journal: Migration to 14.0

--- analytic_journal/__manifest__.py
+++ analytic_journal/__manifest__.py
@@ -1,12 +1,12 @@
 # Copyright 2015 Odoo SA
-# Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
     "name": "Analytic Journal",
-    "version": "10.0.1.0.0",
+    "version": "14.0.1.0.0",
     "summary": "Analytic Journals as in previous Odoo versions",
-    "author": "Odoo SA",
+    "website": "https://github.com/ForgeFlow/eficent-odoo-addons",
+    "author": "ForgeFlow",
     "license": "LGPL-3",
     "category": "Analytic",
     "depends": ["account", "analytic"],
