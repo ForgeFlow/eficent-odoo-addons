4c95b1dfc0bc1a697b9c6333ccaa44fee095eb04
JasminSForgeFlow <jasmin.solanki@forgeflow.com>
2023-02-21 15:26:16 +0530

    [IMP] analytic_plan: black, isort, prettier

--- analytic_plan/models/product.py
+++ analytic_plan/models/product.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 #    Copyright 2015 Matmoz d.o.o. (Matjaž Mozetič)
 #    Copyright 2015 Eficent (Jordi Ballester Alomar)
 #    Copyright 2017 Luxim d.o.o. (Matjaž Mozetič)
@@ -11,13 +10,11 @@ class ProductTemplate(models.Model):
     _inherit = "product.template"
 
     revenue_analytic_plan_journal_id = fields.Many2one(
-        comodel_name='account.analytic.plan.journal',
-        string='Revenue Plan Journal',
-        ondelete='restrict'
+        comodel_name="account.analytic.plan.journal",
+        string="Revenue Plan Journal",
+        ondelete="restrict",
     )
 
     expense_analytic_plan_journal_id = fields.Many2one(
-        'account.analytic.plan.journal',
-        'Expense Plan Journal',
-        ondelete='restrict'
+        "account.analytic.plan.journal", "Expense Plan Journal", ondelete="restrict"
     )
