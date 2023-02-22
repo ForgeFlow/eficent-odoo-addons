50df19f0133ccfd2614ead2cb7433498d9f6b601
JasminSForgeFlow <jasmin.solanki@forgeflow.com>
2023-02-22 11:52:02 +0530

    [MIG] analytic_plan: Migration to 14.0

--- analytic_plan/models/account_analytic_plan_journal.py
+++ analytic_plan/models/account_analytic_plan_journal.py
@@ -1,5 +1,3 @@
-# Copyright 2015 Eficent Business and IT Consulting Services S.L.
-# (Jordi Ballester Alomar)
 # Copyright 2016 Matmoz d.o.o.
 # (Matjaž Mozetič)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
@@ -8,7 +6,6 @@ from odoo import fields, models
 
 
 class AccountAnalyticPlanJournal(models.Model):
-
     _name = "account.analytic.plan.journal"
     _description = "Analytic Journal Plan"
 
--- analytic_plan/models/account_analytic_plan_version.py
+++ analytic_plan/models/account_analytic_plan_version.py
@@ -1,5 +1,3 @@
-# Copyright 2015 Eficent Business and IT Consulting Services S.L.
-# (Jordi Ballester Alomar)
 # Copyright 2016 Matmoz d.o.o.
 # (Matjaž Mozetič)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
@@ -32,19 +30,17 @@ class AccountAnalyticPlanVersion(models.Model):
     )
     default_plan = fields.Boolean("Default planning version", default=False)
 
-    @api.multi
     @api.constrains("default_committed")
     def _check_default_committed(self):
-        for rec in self:
+        for _rec in self:
             default_committed = self.search([("default_committed", "=", True)])
             if len(default_committed) > 1:
                 raise UserError(_("Only one default commitments version can exist."))
             return
 
-    @api.multi
     @api.constrains("default_plan")
     def _check_default_plan(self):
-        for rec in self:
+        for _rec in self:
             default_plan = self.search([("default_plan", "=", True)])
             if len(default_plan) > 1:
                 raise UserError(_("Only one default plan version can exist."))
