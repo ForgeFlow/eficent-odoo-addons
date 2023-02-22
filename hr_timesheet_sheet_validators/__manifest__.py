ebec1c6f2913804acf67bf8e0ca0b8d6ce271262
Juany Davila <juanyadavilap@gmail.com>
2023-02-21 20:05:12 -0500

    [MIG] hr_timesheet_sheet_validators: migration to 15.0

--- hr_timesheet_sheet_validators/__manifest__.py
+++ hr_timesheet_sheet_validators/__manifest__.py
@@ -2,7 +2,7 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
 {
     "name": "HR Timesheet Sheet Validators",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "ForgeFlow",
     "category": "Human Resources",
     "summary": """This module allows a user outside of the Human Resources
