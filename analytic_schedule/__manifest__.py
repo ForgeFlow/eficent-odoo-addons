7f89a2d837da570e0dd3f35693e4d2428787d38d
Dhaval Talpada <dhaval.talpada@forgeflow.com>
2023-02-25 04:45:19 +0530

    [MIG] analytic_schedule: Migration to 14.0

--- analytic_schedule/__manifest__.py
+++ analytic_schedule/__manifest__.py
@@ -1,16 +1,17 @@
-# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017-23 ForgeFlow S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Analytic Schedule",
-    "version": "10.0.2.0.1",
+    "version": "14.0.1.0.0",
     "summary": "Automatically computes start and end dates for analytic "
     "accounts based on the earliest start and latest finish date "
     "of the children.",
-    "author": "Eficent",
+    "author": "ForgeFlow",
+    "license": "AGPL-3",
     "website": "https://github.com/ForgeFlow/eficent-odoo-addons",
     "category": "Generic Modules/Projects & Services",
-    "depends": ["analytic", "account_analytic_parent"],
+    "depends": ["project", "analytic", "account_analytic_parent"],
     "data": ["views/analyltic_account_view.xml"],
     "installable": True,
 }
