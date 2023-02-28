7d9da3960cd744d39a09e3811bf5fa06a9c4d93a
Juany Davila <juanyadavilap@gmail.com>
2023-02-27 21:21:08 -0500

    [IMP]account_analytic_journal_lock_to_date: black, isort, prettier

--- account_analytic_journal_lock_to_date/__manifest__.py
+++ account_analytic_journal_lock_to_date/__manifest__.py
@@ -1,21 +1,17 @@
-# -*- coding: utf-8 -*-
 # Copyright 2021 ForgeFlow S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Account Journal Lock Date',
-    'summary': """
+    "name": "Account Journal Lock Date",
+    "summary": """
         Lock each journal independently""",
-    'version': '10.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'ForgeFlow S.L.,Odoo Community Association (OCA)',
-    'website': 'https://acsone.eu/',
-    'depends': [
-        'analytic_journal',
-        'account_analytic_parent'
-    ],
-    'data': [
-        'views/account_analytic_journal.xml',
-        'views/analytic_account_view.xml',
+    "version": "10.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ForgeFlow S.L.,Odoo Community Association (OCA)",
+    "website": "https://github.com/ForgeFlow/eficent-odoo-addons",
+    "depends": ["analytic_journal", "account_analytic_parent"],
+    "data": [
+        "views/account_analytic_journal.xml",
+        "views/analytic_account_view.xml",
     ],
 }
--- account_analytic_journal_lock_to_date/models/account_analytic_account.py
+++ account_analytic_journal_lock_to_date/models/account_analytic_account.py
@@ -1,20 +1,19 @@
-# -*- coding: utf-8 -*-
 # Copyright 2021 ForgeFlow S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, models, fields
+from odoo import api, fields, models
 
 
 class AccountAnalyticAccount(models.Model):
-    _inherit = 'account.analytic.account'
+    _inherit = "account.analytic.account"
 
     surpass_lock_dates = fields.Boolean()
 
     @api.multi
     def write(self, vals):
         # propagate surpass_lock_dates in the hierarchy
-        if self and 'surpass_lock_dates' in vals and not vals['surpass_lock_dates']:
-            self.mapped('child_ids').write({'surpass_lock_dates': False})
-        elif self and 'surpass_lock_dates' in vals and vals['surpass_lock_dates']:
-            self.mapped('child_ids').write({'surpass_lock_dates': True})
+        if self and "surpass_lock_dates" in vals and not vals["surpass_lock_dates"]:
+            self.mapped("child_ids").write({"surpass_lock_dates": False})
+        elif self and "surpass_lock_dates" in vals and vals["surpass_lock_dates"]:
+            self.mapped("child_ids").write({"surpass_lock_dates": True})
         return super(AccountAnalyticAccount, self).write(vals)
