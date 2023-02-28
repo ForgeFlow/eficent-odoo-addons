51b4e297996c998ee521c1df56f8ad8e35092f82
Dhaval Talpada <dhaval.talpada@forgeflow.com>
2023-02-28 09:47:40 +0530

    [MIG] analytic_contract_value: Migration to 14.0

--- analytic_contract_value/models/__init__.py
+++ analytic_contract_value/models/__init__.py
@@ -1,4 +1,4 @@
-# © 2015 Eficent - Jordi Ballester Alomar
+# Copyright 2023 ForgeFlow S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from . import account_analytic_account
