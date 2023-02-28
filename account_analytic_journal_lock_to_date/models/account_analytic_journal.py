bd257e359d99d3beab62041dcd301949d5384a56
Juany Davila <juanyadavilap@gmail.com>
2023-02-27 21:21:08 -0500

    [IMP]account_analytic_journal_lock_to_date: black, isort, prettier

--- account_analytic_journal_lock_to_date/models/account_analytic_journal.py
+++ account_analytic_journal_lock_to_date/models/account_analytic_journal.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2021 ForgeFlow S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
@@ -6,11 +5,11 @@ from odoo import fields, models
 
 
 class AccountAnalyticJournal(models.Model):
-    _inherit = 'account.analytic.journal'
+    _inherit = "account.analytic.journal"
 
     restrict_lock_dates = fields.Boolean()
     journal_lock_to_date = fields.Date(
         string="Lock to date",
         help="Moves cannot be entered nor modified in this "
-             "journal prior to the lock to date, if empty tomorrow's date is considered"
+        "journal prior to the lock to date, if empty tomorrow's date is considered",
     )
