2ff7406ee36c1624eb5784872d4da3226bb7d563
Juany Davila <juanyadavilap@gmail.com>
2023-03-01 12:20:04 -0500

    [MIG] account_analytic_cost_category: migration to 15.0

--- account_analytic_cost_category/__init__.py
+++ account_analytic_cost_category/__init__.py
@@ -1,4 +1,4 @@
-# © 2014-17 ForgeFlowS.L.
-# © 2016 Matmoz d.o.o.
+# Copyright 2014-17 ForgeFlow S.L.
+# Copyright 2016 Matmoz d.o.o.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from . import models
