a63ed67a3785110414f809629257669143d86a1f
Juany Davila <juanyadavilap@gmail.com>
2023-03-01 12:11:51 -0500

    [IMP]account_analytic_cost_category: black, isort, prettier

--- account_analytic_cost_category/__manifest__.py
+++ account_analytic_cost_category/__manifest__.py
@@ -1,23 +1,18 @@
-# -*- coding: utf-8 -*-
 # © 2014-17 Eficent Business and IT Consulting Services S.L.
 # © 2016 Matmoz d.o.o.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Analytic Account Cost Category',
-    'version': '10.0.1.0.0',
-    'summary': 'Adds the cost category to analytic accounts.',
-    'author': 'Eficent, Odoo Community Association (OCA),'
-              'Project Expert Team',
-    'website': 'http://project.expert',
-    'category': 'Project Management',
-    'license': 'AGPL-3',
-    'depends': [
-        'account',
-        'analytic'
+    "name": "Analytic Account Cost Category",
+    "version": "10.0.1.0.0",
+    "summary": "Adds the cost category to analytic accounts.",
+    "author": "Eficent, Odoo Community Association (OCA)," "Project Expert Team",
+    "website": "https://github.com/ForgeFlow/eficent-odoo-addons",
+    "category": "Project Management",
+    "license": "AGPL-3",
+    "depends": ["account", "analytic"],
+    "data": [
+        "views/account_analytic_account_view.xml",
     ],
-    'data': [
-        'views/account_analytic_account_view.xml',
-    ],
-    'installable': True,
+    "installable": True,
 }
--- account_analytic_cost_category/models/account_analytic_line.py
+++ account_analytic_cost_category/models/account_analytic_line.py
@@ -1,15 +1,14 @@
-# -*- coding: utf-8 -*-
 # © 2014-17 Eficent Business and IT Consulting Services S.L.
 # © 2016 Matmoz d.o.o.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import models, fields
+from odoo import fields, models
 
 
 class AccountAnalyticLine(models.Model):
 
-    _inherit = 'account.analytic.line'
+    _inherit = "account.analytic.line"
 
     cost_category = fields.Selection(
-        [('cogs', 'Cost of Goods Sold'),
-         ('expense', 'Expense')],
-        related="account_id.cost_category")
+        [("cogs", "Cost of Goods Sold"), ("expense", "Expense")],
+        related="account_id.cost_category",
+    )
