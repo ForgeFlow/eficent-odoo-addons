66aa3dbd5070319049a0aa233085fabd1fac994b
Juany Davila <juanyadavilap@gmail.com>
2023-03-01 12:11:51 -0500

    [IMP]account_analytic_cost_category: black, isort, prettier

--- account_analytic_cost_category/models/account_analytic_account.py
+++ account_analytic_cost_category/models/account_analytic_account.py
@@ -1,19 +1,17 @@
-# -*- coding: utf-8 -*-
 # © 2014-17 Eficent Business and IT Consulting Services S.L.
 # © 2016 Matmoz d.o.o.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import models, fields
+from odoo import fields, models
 
 
 class AccountAnalyticAccount(models.Model):
 
-    _inherit = 'account.analytic.account'
+    _inherit = "account.analytic.account"
 
     cost_category = fields.Selection(
-        [('cogs', 'Cost of Goods Sold'),
-         ('expense', 'Expense')],
-        'Type of Cost',
+        [("cogs", "Cost of Goods Sold"), ("expense", "Expense")],
+        "Type of Cost",
         help="""Defines what type of cost does the analytic account carry
         from an employee perspective.""",
-        default='expense'
+        default="expense",
     )
