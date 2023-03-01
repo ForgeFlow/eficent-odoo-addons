787b627d180d6538e1155470f32ff13cffcb7313
Juany Davila <juanyadavilap@gmail.com>
2023-02-28 23:02:32 -0500

    [IMP]account_analytic_post_by_type: black, isort, prettier

--- account_analytic_post_by_type/models/account_move.py
+++ account_analytic_post_by_type/models/account_move.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
@@ -10,12 +9,11 @@ class AccountMoveLine(models.Model):
 
     @api.multi
     def create_analytic_lines(self):
-        new_lines = self.env['account.move.line']
-        valid_list = ['Income', 'Expense', 'Cost', 'Revenue']
+        new_lines = self.env["account.move.line"]
+        valid_list = ["Income", "Expense", "Cost", "Revenue"]
         for line in self:
             for valid in valid_list:
-                if (line.account_id and
-                        valid in line.account_id.user_type_id.name):
+                if line.account_id and valid in line.account_id.user_type_id.name:
                     new_lines += line
                     break
         return super(AccountMoveLine, new_lines).create_analytic_lines()
