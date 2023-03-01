d79a4eacf78d65c1704a45fabfd2c227f69f1ca1
Juany Davila <juanyadavilap@gmail.com>
2023-02-28 19:41:54 -0500

    [IMP]account_move_category: black, isort, prettier

--- account_move_category/models/account_move.py
+++ account_move_category/models/account_move.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from odoo import fields, models
@@ -8,6 +7,11 @@ class AccountMove(models.Model):
     _inherit = "account.move"
 
     category = fields.Selection(
-        [('normal', 'Normal'),
-         ('open', 'Opening Fiscal year'),
-         ('close', 'Closing Fiscal Year')], 'Category', index=True)
+        [
+            ("normal", "Normal"),
+            ("open", "Opening Fiscal year"),
+            ("close", "Closing Fiscal Year"),
+        ],
+        "Category",
+        index=True,
+    )
