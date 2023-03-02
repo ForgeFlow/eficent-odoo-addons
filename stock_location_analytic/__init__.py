4d290437a8ad426de9a420d0344dd2ad69a3ca6a
AaronHForgeFlow <ahenriquez@eficent.com>
2023-03-02 10:26:38 +0100

    [FIX]stock_location_analytic: Copyright

--- stock_location_analytic/__init__.py
+++ stock_location_analytic/__init__.py
@@ -1,3 +1,3 @@
-# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017-23 ForgeFlow S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from . import models
