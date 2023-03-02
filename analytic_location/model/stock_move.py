d522c11120ad52651418f1ffb9912485f30a962b
Juany Davila <juanyadavilap@gmail.com>
2023-03-01 20:14:17 -0500

    [MIG] analytic_location: migration to 14.0

--- analytic_location/model/stock_move.py
+++ analytic_location/model/stock_move.py
@@ -1,4 +1,4 @@
-# © 2015-19 Eficent Business and IT Consulting Services S.L.
+# © 2015-19 ForgeFlow S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 from odoo import fields, models
 
