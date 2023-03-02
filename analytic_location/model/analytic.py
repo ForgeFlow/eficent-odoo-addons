fab1e0a78619ab3d2b26aa930630429391dd2595
AaronHForgeFlow <ahenriquez@eficent.com>
2023-03-02 10:18:12 +0100

    [IMP]analytic_location: black, isort, prettier

--- analytic_location/model/analytic.py
+++ analytic_location/model/analytic.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2014-17 Eficent Business and IT Consulting Services S.L.
 # Copyright 2016 Matmoz d.o.o.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
@@ -7,34 +6,36 @@ from odoo import api, fields, models
 
 
 class AccountAnalyticAccount(models.Model):
-    _inherit = 'account.analytic.account'
+    _inherit = "account.analytic.account"
 
     @api.model
     def _default_dest_address(self):
-        partner_id = self.env.context.get('partner_id', False)
+        partner_id = self.env.context.get("partner_id", False)
         if partner_id:
-            return self.env['res.partner'].address_get(
-                [partner_id], ['delivery']
-            )['delivery'],
+            return (
+                self.env["res.partner"].address_get([partner_id], ["delivery"])[
+                    "delivery"
+                ],
+            )
         else:
             return False
 
     location_id = fields.Many2one(
-        'stock.location',
-        'Default Stock Location',
-        domain=[('usage', '<>', 'view')]
+        "stock.location", "Default Stock Location", domain=[("usage", "<>", "view")]
     )
     picking_type_id = fields.Many2one(
-        'stock.picking.type',
-        'Default Picking Type for the project',
-        domain="[('code', '=', 'incoming')]")
+        "stock.picking.type",
+        "Default Picking Type for the project",
+        domain="[('code', '=', 'incoming')]",
+    )
     picking_type_out_id = fields.Many2one(
-        'stock.picking.type',
-        'Default Picking Type for deliveries',
-        domain="[('code', '=', 'outgoing')]")
+        "stock.picking.type",
+        "Default Picking Type for deliveries",
+        domain="[('code', '=', 'outgoing')]",
+    )
     dest_address_id = fields.Many2one(
-        'res.partner',
-        'Delivery Address',
+        "res.partner",
+        "Delivery Address",
         default=_default_dest_address,
-        help="""Delivery address for the current contract project."""
+        help="""Delivery address for the current contract project.""",
     )
