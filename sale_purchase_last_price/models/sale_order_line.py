edb441cb3859888026b251f84a2c4d4312637aa3
Juany Davila <juanyadavilap@gmail.com>
2023-03-03 08:12:05 -0500

    [IMP]sale_purchase_last_price: black, isort, prettier

--- sale_purchase_last_price/models/sale_order_line.py
+++ sale_purchase_last_price/models/sale_order_line.py
@@ -1,25 +1,31 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import api, models, fields
+from odoo import api, fields, models
+
 import odoo.addons.decimal_precision as dp
 
 
 class SaleOrderLine(models.Model):
-    _inherit = 'sale.order.line'
+    _inherit = "sale.order.line"
 
     @api.multi
     def _compute_last_purchase(self):
-        """ Get last purchase price, last purchase date and last supplier """
+        """Get last purchase price, last purchase date and last supplier"""
         for so_line in self:
-            date_compare = (so_line.order_id.commitment_date if
-                            so_line.order_id.commitment_date else
-                            so_line.order_id.date_order)
-            po_lines = self.env['purchase.order.line'].search(
-                [('product_id', '=', so_line.product_id.id),
-                 ('date_order', '<=', date_compare),
-                 ('state', 'in', ['purchase', 'done'])],
-                order='id DESC', limit=1)
+            date_compare = (
+                so_line.order_id.commitment_date
+                if so_line.order_id.commitment_date
+                else so_line.order_id.date_order
+            )
+            po_lines = self.env["purchase.order.line"].search(
+                [
+                    ("product_id", "=", so_line.product_id.id),
+                    ("date_order", "<=", date_compare),
+                    ("state", "in", ["purchase", "done"]),
+                ],
+                order="id DESC",
+                limit=1,
+            )
 
             if po_lines:
                 so_line.last_purchase_date = po_lines[0].date_order
@@ -31,13 +37,15 @@ class SaleOrderLine(models.Model):
                 so_line.last_supplier_id = False
 
     last_purchase_price = fields.Float(
-        compute='_compute_last_purchase',
-        digits=dp.get_precision('Product'),
-        string='Last Purchase Price')
+        compute="_compute_last_purchase",
+        digits=dp.get_precision("Product"),
+        string="Last Purchase Price",
+    )
     last_purchase_date = fields.Date(
-        compute='_compute_last_purchase',
-        string='Last Purchase Date')
+        compute="_compute_last_purchase", string="Last Purchase Date"
+    )
     last_supplier_id = fields.Many2one(
-        compute='_compute_last_purchase',
-        comodel_name='res.partner',
-        string='Last Supplier')
+        compute="_compute_last_purchase",
+        comodel_name="res.partner",
+        string="Last Supplier",
+    )
--- sale_purchase_last_price/tests/test_purchase_last_price.py
+++ sale_purchase_last_price/tests/test_purchase_last_price.py
@@ -1,61 +1,75 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo.tests import common
 from datetime import datetime
-from odoo.tools import DEFAULT_SERVER_DATE_FORMAT as DF
-from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT as DT
+
+from odoo.tests import common
+from odoo.tools import (
+    DEFAULT_SERVER_DATE_FORMAT as DF,
+    DEFAULT_SERVER_DATETIME_FORMAT as DT,
+)
 
 
 class TestPurchaseLastPrice(common.TransactionCase):
-
     def setUp(self):
         super(TestPurchaseLastPrice, self).setUp()
-        self.purchase_model = self.env['purchase.order']
-        self.purchase_line_model = self.env['purchase.order.line']
-        self.location = self.env.ref('stock.stock_location_suppliers')
-        self.sale_order_model = self.env['sale.order']
-        self.sale_line_model = self.env['sale.order.line']
-        self.pricelist = self.env.ref('product.list0')
-        partner_values = {'name': 'Peace and love'}
-        self.partner = self.env['res.partner'].create(partner_values)
-        self.partner_p = self.env.ref('base.res_partner_1')
-        product_values = {'name': 'Gun',
-                          'list_price': 5,
-                          'type': 'product'}
-        self.product = self.env['product.product'].create(product_values)
-        self.product_uom_unit = self.env.ref('product.product_uom_unit')
+        self.purchase_model = self.env["purchase.order"]
+        self.purchase_line_model = self.env["purchase.order.line"]
+        self.location = self.env.ref("stock.stock_location_suppliers")
+        self.sale_order_model = self.env["sale.order"]
+        self.sale_line_model = self.env["sale.order.line"]
+        self.pricelist = self.env.ref("product.list0")
+        partner_values = {"name": "Peace and love"}
+        self.partner = self.env["res.partner"].create(partner_values)
+        self.partner_p = self.env.ref("base.res_partner_1")
+        product_values = {"name": "Gun", "list_price": 5, "type": "product"}
+        self.product = self.env["product.product"].create(product_values)
+        self.product_uom_unit = self.env.ref("product.product_uom_unit")
 
     def create_sale_order(self):
-        sale_obj = self.env['sale.order']
+        sale_obj = self.env["sale.order"]
         values = {
-            'partner_id': self.partner.id,
-            'commitment_date': '02-02-2010',
-            'order_line': [(0, 0, {
-                'name': self.product.name,
-                'product_id': self.product.id,
-                'product_uom': self.product_uom_unit.id,
-                'price_unit': 234.0,
-                'product_uom_qty': 1})],
+            "partner_id": self.partner.id,
+            "commitment_date": "02-02-2010",
+            "order_line": [
+                (
+                    0,
+                    0,
+                    {
+                        "name": self.product.name,
+                        "product_id": self.product.id,
+                        "product_uom": self.product_uom_unit.id,
+                        "price_unit": 234.0,
+                        "product_uom_qty": 1,
+                    },
+                )
+            ],
         }
         return sale_obj.create(values)
 
     def test_purchase_last_price(self):
-        purchase_order = self.purchase_model.create({
-            'partner_id': self.partner_p.id,
-            'location_id': self.location.id,
-            'pricelist_id': self.pricelist.id,
-            'date': '01-01-2010',
-            'order_line': [(0, 0, {
-                'product_id': self.product.id,
-                'price_unit': 123.0,
-                'product_uom': self.env.ref('product.product_uom_unit').id,
-                'product_qty': 5.0,
-                'name': self.product.name,
-                'date_planned': '01-01-2010'
-            })]
-        })
+        purchase_order = self.purchase_model.create(
+            {
+                "partner_id": self.partner_p.id,
+                "location_id": self.location.id,
+                "pricelist_id": self.pricelist.id,
+                "date": "01-01-2010",
+                "order_line": [
+                    (
+                        0,
+                        0,
+                        {
+                            "product_id": self.product.id,
+                            "price_unit": 123.0,
+                            "product_uom": self.env.ref("product.product_uom_unit").id,
+                            "product_qty": 5.0,
+                            "name": self.product.name,
+                            "date_planned": "01-01-2010",
+                        },
+                    )
+                ],
+            }
+        )
         purchase_order.button_confirm()
         sale = self.create_sale_order()
         p_date = datetime.strptime(purchase_order.date_order, DT)
