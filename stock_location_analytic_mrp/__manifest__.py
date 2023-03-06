60098d1ca2da19b9ca9e25828b34fff11223bb48
JasminSForgeFlow <jasmin.solanki@forgeflow.com>
2023-03-06 09:03:44 +0530

    [MIG] stock_location_analytic_mrp: Migration to 14.0

--- stock_location_analytic_mrp/__manifest__.py
+++ stock_location_analytic_mrp/__manifest__.py
@@ -1,10 +1,10 @@
-# © 2017 Eficent Business and IT Consulting Services S.L.
+# © 2023 ForgeFlow S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Stock Location Analytic MRP",
-    "version": "10.0.1.0.0",
-    "author": "Eficent",
+    "version": "14.0.1.0.0",
+    "author": "ForgeFlow",
     "summary": "MRP analytic location with analytic locations",
     "license": "AGPL-3",
     "website": "https://github.com/ForgeFlow/eficent-odoo-addons",
