e8d25d1363001525ca900ccbfa8790e43ef2f4b9
JasminSForgeFlow <jasmin.solanki@forgeflow.com>
2023-03-07 09:33:56 +0530

    [MIG] stock_location_analytic_change_product_qty: Migration to 14.0

--- stock_location_analytic_change_product_qty/wizard/stock_change_product_qty.py
+++ stock_location_analytic_change_product_qty/wizard/stock_change_product_qty.py
@@ -1,4 +1,4 @@
-# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2023 ForgeFlow S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from odoo import api, fields, models
 
@@ -6,23 +6,18 @@ from odoo import api, fields, models
 class StockChangeProductQty(models.TransientModel):
     _inherit = "stock.change.product.qty"
 
-    @api.multi
-    @api.depends("location_id")
+    @api.depends("product_id")
     def _compute_analytic_account_id(self):
         for wiz in self:
-            if wiz.location_id.analytic_account_id:
-                wiz.analytic_account_id = wiz.location_id.analytic_account_id.id
+            warehouse = self.env["stock.warehouse"].search(
+                [("company_id", "=", self.env.company.id)], limit=1
+            )
+            if warehouse.lot_stock_id.analytic_account_id:
+                wiz.analytic_account_id = warehouse.lot_stock_id.analytic_account_id.id
         return True
 
     analytic_account_id = fields.Many2one(
         comodel_name="account.analytic.account",
         compute=_compute_analytic_account_id,
         string="Analytic Account",
-    )
-
-    location_id = fields.Many2one(
-        "stock.location",
-        "Location",
-        required=True,
-        domain="[('usage', 'in',['internal','customer'])]",
     )
