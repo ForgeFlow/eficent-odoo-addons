b6b6f73b1dbae2fe896bb06f6ff80a887771b0a2
Juany Davila <juanyadavilap@gmail.com>
2023-03-10 08:41:17 -0500

    [MIG] account_analytic_journal_lock_to_date: migration to 15.0

--- account_analytic_journal_lock_to_date/__manifest__.py
+++ account_analytic_journal_lock_to_date/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Account Journal Lock Date",
     "summary": """
         Lock each journal independently""",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "author": "ForgeFlow ,Odoo Community Association (OCA)",
     "website": "https://github.com/ForgeFlow/eficent-odoo-addons",
