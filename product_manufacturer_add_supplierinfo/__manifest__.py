ea0354d937eafb4de659414e05880556bcc572d1
Juany Davila <juanyadavilap@gmail.com>
2023-03-11 18:09:52 -0500

    [IMP] product_manufacturer_add_supplierinfo: black, isort, prettier

--- product_manufacturer_add_supplierinfo/__manifest__.py
+++ product_manufacturer_add_supplierinfo/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # © 2015-17 Eficent Business and IT Consulting Services S.L.
 # - Jordi Ballester Alomar
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
@@ -9,14 +8,11 @@
                added as a vendor in the supplier info.""",
     "version": "10.0.1.0.0",
     "author": "Eficent Business and IT Consulting Services S.L,"
-              "Serpent Consulting Services Pvt. Ltd.,"
-              "Odoo Community Association (OCA)",
-    "website": "www.eficent.com",
+    "Serpent Consulting Services Pvt. Ltd.,"
+    "Odoo Community Association (OCA)",
+    "website": "https://github.com/ForgeFlow/eficent-odoo-addons",
     "category": "Purchase Management",
     "license": "LGPL-3",
-    "depends": [
-        "product",
-        "product_manufacturer"
-    ],
-    'installable': True,
+    "depends": ["product", "product_manufacturer"],
+    "installable": True,
 }
