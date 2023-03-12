6023ae9135b19eb6e94cb1f4f63491db20692326
Juany Davila <juanyadavilap@gmail.com>
2023-03-11 19:16:53 -0500

    [MIG] product_manufacturer_partner: migration to 14.0

--- product_manufacturer_partner/__init__.py
+++ product_manufacturer_partner/__init__.py
@@ -1,3 +1,3 @@
-# © 2015-17 Eficent Business and IT Consulting Services S.L.
+# Copyright 2015-17 ForgeFlow S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 from . import model
