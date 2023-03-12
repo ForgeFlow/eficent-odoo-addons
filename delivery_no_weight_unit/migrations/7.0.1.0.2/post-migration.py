5de13d390c92b340e80b91247646408e91364619
Juany Davila <juanyadavilap@gmail.com>
2023-03-11 19:39:10 -0500

    [IMP] delivery_no_weight_unit: black, isort, prettier

--- delivery_no_weight_unit/migrations/7.0.1.0.2/post-migration.py
+++ delivery_no_weight_unit/migrations/7.0.1.0.2/post-migration.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2011-17 Camptocamp SA
 # Author: Nicolas Bessi.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
@@ -7,8 +6,6 @@
 def migrate(cr, version):
     if not version:
         # only run at first install
-        cr.execute("UPDATE stock_move "
-                   " SET weight_uom_id = NULL;")
+        cr.execute("UPDATE stock_move " " SET weight_uom_id = NULL;")
 
-        cr.execute("UPDATE stock_picking"
-                   " SET weight_uom_id = NULL;")
+        cr.execute("UPDATE stock_picking" " SET weight_uom_id = NULL;")
