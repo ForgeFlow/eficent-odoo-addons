8cba5c78879976640c1f9f943205115d82f129ae
Juany Davila <juanyadavilap@gmail.com>
2023-03-11 19:43:11 -0500

    [MIG] delivery_no_weight_unit: migration to 14.0

--- delivery_no_weight_unit/__manifest__.py
+++ delivery_no_weight_unit/__manifest__.py
@@ -1,12 +1,11 @@
-# Copyright 2015-17 Eficent Business and IT Consulting Services S.L.
-#        <contact@eficent.com>
+# Copyright 2015-17 ForgeFlow S.L. <contact@forgeflow.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Delivery No Weight Unit",
-    "version": "10.0.1.0.0",
+    "version": "14.0.1.0.0",
     "summary": """Delivery No Weight Unit""",
-    "author": "Eficent",
+    "author": "ForgeFlow",
     "website": "https://github.com/ForgeFlow/eficent-odoo-addons",
     "license": "AGPL-3",
     "category": "Purchase Management",
