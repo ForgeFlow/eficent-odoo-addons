c097a14e946f0b6b7fd84f8e266be6bc5e385661
Juany Davila <juanyadavilap@gmail.com>
2023-03-11 19:12:02 -0500

    [IMP] product_manufacturer_partner: black, isort, prettier

--- product_manufacturer_partner/__manifest__.py
+++ product_manufacturer_partner/__manifest__.py
@@ -1,24 +1,20 @@
-# -*- coding: utf-8 -*-
 # © 2015-17 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 {
-    'name': 'Products Manufacturer Partner',
-    'version': '10.0.1.0.0',
-    'author': 'Eficent',
-    "website": "www.eficent.com",
-    'category': 'Purchase Management',
-    'summary': """
+    "name": "Products Manufacturer Partner",
+    "version": "10.0.1.0.0",
+    "author": "Eficent",
+    "website": "https://github.com/ForgeFlow/eficent-odoo-addons",
+    "category": "Purchase Management",
+    "summary": """
     Classify manufacturers by providing an indicator in the partner.
     In the product, select a manufacturer from the list of partners where
     this indicator has been set.
      """,
-    'depends': [
-        'purchase',
-        'product_manufacturer'
+    "depends": ["purchase", "product_manufacturer"],
+    "data": [
+        "view/partner_view.xml",
+        "view/product_view.xml",
     ],
-    'data': [
-        'view/partner_view.xml',
-        'view/product_view.xml',
-    ],
-    'installable': True,
+    "installable": True,
 }
--- product_manufacturer_partner/model/partner.py
+++ product_manufacturer_partner/model/partner.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # © 2015-17 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
-    manufacturer = fields.Boolean('Manufacturer')
+    manufacturer = fields.Boolean("Manufacturer")
