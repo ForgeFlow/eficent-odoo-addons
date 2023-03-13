5f1ee64feb16493148e8ef8f296e8ca452376037
Juany Davila <juanyadavilap@gmail.com>
2023-03-13 16:24:39 -0500

    [MIG] project_stock: migration to 15.0

--- project_stock/__manifest__.py
+++ project_stock/__manifest__.py
@@ -1,10 +1,10 @@
-# © 2017 ForgeFlow S.L.
+# Copyright 2017 ForgeFlow S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 
 {
     "name": "Project Stock Move Link",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "ForgeFlow",
     "license": "AGPL-3",
     "website": "https://github.com/ForgeFlow/eficent-odoo-addons",
