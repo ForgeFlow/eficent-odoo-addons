976a05bab99b6ccf42ecdfa5628718324749b389
Juany Davila <juanyadavilap@gmail.com>
2023-03-13 16:12:14 -0500

    [MIG] project_stock: migration to 14.0

--- project_stock/__init__.py
+++ project_stock/__init__.py
@@ -1,2 +1,2 @@
-# © 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017 ForgeFlow S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
--- project_stock/__manifest__.py
+++ project_stock/__manifest__.py
@@ -1,11 +1,12 @@
-# © 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017 ForgeFlow S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 
 {
     "name": "Project Stock Move Link",
-    "version": "10.0.1.0.0",
-    "author": "Eficent",
+    "version": "14.0.1.0.0",
+    "author": "ForgeFlow",
+    "license": "AGPL-3",
     "website": "https://github.com/ForgeFlow/eficent-odoo-addons",
     "category": "Generic Modules/Projects & Services",
     "depends": ["project", "stock_analytic_account"],
