c652a0d6784225be660fcbc32be766e5473f3584
Juany Davila <juanyadavilap@gmail.com>
2023-03-13 16:08:26 -0500

    [IMP] project_stock: black, isort, prettier

--- project_stock/__manifest__.py
+++ project_stock/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # © 2017 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
@@ -7,17 +6,13 @@
     "name": "Project Stock Move Link",
     "version": "10.0.1.0.0",
     "author": "Eficent",
-    "website": "www.eficent.com",
+    "website": "https://github.com/ForgeFlow/eficent-odoo-addons",
     "category": "Generic Modules/Projects & Services",
-    "depends": [
-        "project",
-        "stock_analytic_account"
-    ],
+    "depends": ["project", "stock_analytic_account"],
     "Summary": """list the Stock Moves associated to the selected project""",
     "data": [
         "view/project_view.xml",
         "security/ir.model.access.csv",
     ],
-    'installable': True,
-
+    "installable": True,
 }
