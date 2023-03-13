5b06fb793e992089243c5e4f1eb5ec85367984eb
Juany Davila <juanyadavilap@gmail.com>
2023-03-13 16:31:15 -0500

    [IMP] purchase_request_comment: black, isort, prettier

--- purchase_request_comment/models/purchase_request.py
+++ purchase_request_comment/models/purchase_request.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2019 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0).
 
@@ -6,10 +5,10 @@ from odoo import fields, models
 
 
 class PurchaseRequest(models.Model):
-    _inherit = 'purchase.request'
+    _inherit = "purchase.request"
 
     internal_comments = fields.Text(
-        string='Internal Comments',
+        string="Internal Comments",
         help="These comments will be propagated to the Purchase Order. "
-             "Text introduced here is internal and merely informative."
+        "Text introduced here is internal and merely informative.",
     )
--- purchase_request_comment/wizard/purchase_request_line_make_purchase_order.py
+++ purchase_request_comment/wizard/purchase_request_line_make_purchase_order.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2019 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0).
 
@@ -10,19 +9,23 @@ class PurchaseRequestLineMakePurchaseOrder(models.TransientModel):
 
     @api.multi
     def make_purchase_order(self):
-        res = super(PurchaseRequestLineMakePurchaseOrder,
-                    self).make_purchase_order()
-        for po_id in set(res.get('domain')[0][2]):
-            purchase = self.env['purchase.order'].browse(po_id)
+        res = super(PurchaseRequestLineMakePurchaseOrder, self).make_purchase_order()
+        for po_id in set(res.get("domain")[0][2]):
+            purchase = self.env["purchase.order"].browse(po_id)
             po_comment = purchase.internal_comments
-            purchase_requests = self.item_ids.mapped('request_id')
+            purchase_requests = self.item_ids.mapped("request_id")
             for pr in purchase_requests:
                 if pr.internal_comments:
                     if not po_comment:
                         po_comment = "Purchase Request [%s]: %s" % (
-                            pr.name, pr.internal_comments)
+                            pr.name,
+                            pr.internal_comments,
+                        )
                     else:
                         po_comment = "%s\nPurchase Request [%s]: %s" % (
-                            po_comment, pr.name, pr.internal_comments)
+                            po_comment,
+                            pr.name,
+                            pr.internal_comments,
+                        )
             purchase.internal_comments = po_comment
         return res
