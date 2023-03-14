38bb31ca8a04ea5b74603a23eb34b1eb0498c1a8
JasminSForgeFlow <jasmin.solanki@forgeflow.com>
2023-03-14 15:20:34 +0530

    [MIG] stock_analytic_account: Migration to 15.0

--- stock_analytic_account/__manifest__.py
+++ stock_analytic_account/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Stock Analytic Account",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "ForgeFlow, Odoo Community Association (OCA)," "Project Expert Team",
     "website": "https://github.com/ForgeFlow/eficent-odoo-addons",
     "category": "Project Management",
