58192e06c0b22989c3d534080664aef1649a736e
Juany Davila <juanyadavilap@gmail.com>
2023-03-15 12:19:44 -0500

    [IMP] analytic_account_open: black, isort, prettier

--- analytic_account_open/wizards/analytic_account_open.py
+++ analytic_account_open/wizards/analytic_account_open.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 Eficent - Jordi Ballester Alomar
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
@@ -6,25 +5,21 @@ from odoo import api, fields, models
 
 
 class AnalyticAccountOpen(models.TransientModel):
-    _name = 'analytic.account.open'
-    _description = 'Open single analytic account'
+    _name = "analytic.account.open"
+    _description = "Open single analytic account"
 
     analytic_account_id = fields.Many2one(
-        'account.analytic.account',
-        'Analytic Account',
-        required=True
+        "account.analytic.account", "Analytic Account", required=True
     )
-    include_child = fields.Boolean(
-        'Include child accounts',
-        default=True
-    )
+    include_child = fields.Boolean("Include child accounts", default=True)
 
     @api.model
     def _get_child_analytic_accounts(self, curr_id):
         result = {}
         result[curr_id] = True
         # Now add the children
-        self.env.cr.execute('''
+        self.env.cr.execute(
+            """
         WITH RECURSIVE children AS (
         SELECT parent_id, id
         FROM account_analytic_account
@@ -35,7 +30,9 @@ class AnalyticAccountOpen(models.TransientModel):
         JOIN children b ON(a.parent_id = b.id)
         )
         SELECT * FROM children order by parent_id
-        ''', (curr_id,))
+        """,
+            (curr_id,),
+        )
         res = self.env.cr.fetchall()
         for x, y in res:
             result[y] = True
@@ -44,8 +41,7 @@ class AnalyticAccountOpen(models.TransientModel):
     @api.multi
     def analytic_account_open_window(self):
         self.ensure_one()
-        act_window_id = self.env.ref(
-            'analytic.action_account_analytic_account_form')
+        act_window_id = self.env.ref("analytic.action_account_analytic_account_form")
         result = act_window_id.read()[0]
         acc_id = self.analytic_account_id.id
         acc_ids = []
@@ -53,5 +49,5 @@ class AnalyticAccountOpen(models.TransientModel):
             acc_ids = self._get_child_analytic_accounts(acc_id)
         else:
             acc_ids.append(acc_id)
-        result['domain'] = "[('id','in', ["+','.join(map(str, acc_ids))+"])]"
+        result["domain"] = "[('id','in', [" + ",".join(map(str, acc_ids)) + "])]"
         return result
