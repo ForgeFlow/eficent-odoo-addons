49db369ab373bde234275ff98bd10fbeef1f0060
Juany Davila <juanyadavilap@gmail.com>
2023-03-15 12:01:43 -0500

    [IMP] account_payment_order_partner: black, isort, prettier

--- account_payment_order_partner/__manifest__.py
+++ account_payment_order_partner/__manifest__.py
@@ -1,20 +1,18 @@
-# -*- coding: utf-8 -*-
 # © 2018 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Account Payment Order Partner',
-    'version': '10.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': "Eficent, "
-              "Odoo Community Association (OCA)",
-    'website': 'https://github.com/OCA/bank-payment',
-    'category': 'Banking addons',
-    'depends': [
-        'account_payment_order',
+    "name": "Account Payment Order Partner",
+    "version": "10.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Eficent, " "Odoo Community Association (OCA)",
+    "website": "https://github.com/ForgeFlow/eficent-odoo-addons",
+    "category": "Banking addons",
+    "depends": [
+        "account_payment_order",
     ],
-    'data': [
-        'wizard/account_payment_line_create_view.xml',
+    "data": [
+        "wizard/account_payment_line_create_view.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
--- account_payment_order_partner/wizard/account_payment_line_create.py
+++ account_payment_order_partner/wizard/account_payment_line_create.py
@@ -1,21 +1,19 @@
-# -*- coding: utf-8 -*-
 # © 2009 EduSense BV (<http://www.edusense.nl>)
 # © 2011-2013 Therp BV (<http://therp.nl>)
 # © 2014-2015 ACSONE SA/NV (<http://acsone.eu>)
 # © 2015-2016 Akretion (<http://www.akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class AccountPaymentLineCreate(models.TransientModel):
-    _inherit = 'account.payment.line.create'
+    _inherit = "account.payment.line.create"
 
-    partner_id = fields.Many2one(
-        'res.partner', string='Partner')
+    partner_id = fields.Many2one("res.partner", string="Partner")
 
     @api.multi
     def _prepare_move_line_domain(self):
         res = super(AccountPaymentLineCreate, self)._prepare_move_line_domain()
-        res.append(('partner_id', '=', self.partner_id.id))
+        res.append(("partner_id", "=", self.partner_id.id))
         return res
