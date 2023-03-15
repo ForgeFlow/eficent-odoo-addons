7ded180ffd8aff5aaf7fa809caba121f7070464b
Juany Davila <juanyadavilap@gmail.com>
2023-03-15 12:14:04 -0500

    [MIG] account_payment_order_partner: migration to 15.0

--- account_payment_order_partner/__manifest__.py
+++ account_payment_order_partner/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Account Payment Order Partner",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "author": "ForgeFlow, " "Odoo Community Association (OCA)",
     "website": "https://github.com/ForgeFlow/eficent-odoo-addons",
