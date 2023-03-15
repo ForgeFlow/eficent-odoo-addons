304a03b27fdd6e2d0e1d1eb249be5c4cf8886623
JasminSForgeFlow <jasmin.solanki@forgeflow.com>
2023-03-15 17:17:23 +0530

    [IMP] project_location: black, isort, prettier

--- project_location/__manifest__.py
+++ project_location/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # © 2017 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
@@ -6,14 +5,11 @@
     "name": "Stock Location in Project",
     "version": "10.0.1.0.0",
     "author": "Eficent, Odoo Community Association (OCA)",
-    "license": 'AGPL-3',
-    "website": "www.eficent.com",
+    "license": "AGPL-3",
+    "website": "https://github.com/ForgeFlow/eficent-odoo-addons",
     "category": "Generic Modules/Projects & Services",
     "summary": """stock location in the project.""",
-    "depends": [
-        "project_stock",
-        "analytic_location"
-    ],
+    "depends": ["project_stock", "analytic_location"],
     "data": [
         "view/project_view.xml",
         "security/ir.model.access.csv",
