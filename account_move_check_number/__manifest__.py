5293635b0755310d9fb9657b26693930a64a2cea
Juany Davila <juanyadavilap@gmail.com>
2023-03-15 11:49:15 -0500

    [MIG] account_move_check_number: migration to 15.0

--- account_move_check_number/__manifest__.py
+++ account_move_check_number/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Account Move Check Number",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "author": "ForgeFlow, " "Odoo Community Association (OCA)",
     "website": "https://github.com/ForgeFlow/eficent-odoo-addons",
--- account_move_check_number/models/account_move_line.py
+++ account_move_check_number/models/account_move_line.py
@@ -7,4 +7,4 @@ from odoo import fields, models
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
 
-    check_number = fields.Integer(related="payment_id.check_number", store=True)
+    check_number = fields.Char(related="payment_id.check_number", store=True)
