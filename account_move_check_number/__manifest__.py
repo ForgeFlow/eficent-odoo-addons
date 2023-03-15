ae99d624d41a6b6f998fb5fca425ddd9b580085f
Juany Davila <juanyadavilap@gmail.com>
2023-03-15 11:43:05 -0500

    [IMP] account_move_check_number: black, isort, prettier

--- account_move_check_number/__manifest__.py
+++ account_move_check_number/__manifest__.py
@@ -1,22 +1,20 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lpgl.html).
 
 
 {
-    'name': 'Account Move Check Number',
-    'version': '10.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': "Eficent, "
-              "Odoo Community Association (OCA)",
-    'website': 'https://github.com/OCA/bank-payment',
-    'category': 'Banking addons',
-    'depends': [
-        'account',
-        'account_check_printing',
+    "name": "Account Move Check Number",
+    "version": "10.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Eficent, " "Odoo Community Association (OCA)",
+    "website": "https://github.com/ForgeFlow/eficent-odoo-addons",
+    "category": "Banking addons",
+    "depends": [
+        "account",
+        "account_check_printing",
     ],
-    'data': [
-        'views/account_move_view.xml',
+    "data": [
+        "views/account_move_view.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
