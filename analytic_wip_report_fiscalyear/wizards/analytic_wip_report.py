6b13265861698c109410499799699f6bf14a8f74
JasminSForgeFlow <jasmin.solanki@forgeflow.com>
2023-03-16 15:37:29 +0530

    [IMP] analytic_wip_report_fiscalyear: black, isort, prettier

--- analytic_wip_report_fiscalyear/wizards/analytic_wip_report.py
+++ analytic_wip_report_fiscalyear/wizards/analytic_wip_report.py
@@ -1,38 +1,36 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from odoo import api, fields, models
 
 
 class AnalyticWipReport(models.TransientModel):
-    _inherit = 'analytic.wip.report'
+    _inherit = "analytic.wip.report"
 
     fiscalyear_id = fields.Many2one(
-        'date.range', required=True,
-        domain=[('type_id.fiscal_year', '=', True)])
-    from_date_fy = fields.Date('From (within the fiscal year)', required=True)
-    to_date_fy = fields.Date('To (within the fiscal year)', required=True)
+        "date.range", required=True, domain=[("type_id.fiscal_year", "=", True)]
+    )
+    from_date_fy = fields.Date("From (within the fiscal year)", required=True)
+    to_date_fy = fields.Date("To (within the fiscal year)", required=True)
 
     @api.multi
     def analytic_wip_report_open_window(self):
         result_context = {}
-        result = super(AnalyticWipReport, self).\
-            analytic_wip_report_open_window()
+        result = super(AnalyticWipReport, self).analytic_wip_report_open_window()
         data = self.read()[0]
-        if data['from_date']:
-            result_context.update({'from_date': data['from_date']})
-        if data['to_date']:
-            result_context.update({'to_date': data['to_date']})
-        if data['fiscalyear_id']:
-            result_context.update({'fiscalyear_id': data['fiscalyear_id']})
-        if data['from_date_fy']:
-            result_context.update({'from_date_fy': data['from_date_fy']})
-        if data['to_date_fy']:
-            result_context.update({'to_date_fy': data['to_date_fy']})
-        result['context'] = str(result_context)
+        if data["from_date"]:
+            result_context.update({"from_date": data["from_date"]})
+        if data["to_date"]:
+            result_context.update({"to_date": data["to_date"]})
+        if data["fiscalyear_id"]:
+            result_context.update({"fiscalyear_id": data["fiscalyear_id"]})
+        if data["from_date_fy"]:
+            result_context.update({"from_date_fy": data["from_date_fy"]})
+        if data["to_date_fy"]:
+            result_context.update({"to_date_fy": data["to_date_fy"]})
+        result["context"] = str(result_context)
         return result
 
-    @api.onchange('fiscalyear_id')
+    @api.onchange("fiscalyear_id")
     def onchange_fiscalyear_id(self):
         for rec in self:
             if rec.fiscalyear_id:
