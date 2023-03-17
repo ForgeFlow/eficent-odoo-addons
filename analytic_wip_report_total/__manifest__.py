88199b41c39c1b2866eaf9e5eebbd03f2b398e77
JasminSForgeFlow <jasmin.solanki@forgeflow.com>
2023-03-17 16:41:27 +0530

    [MIG] analytic_wip_report_total: Migration to 14.0

--- analytic_wip_report_total/__manifest__.py
+++ analytic_wip_report_total/__manifest__.py
@@ -1,11 +1,11 @@
-# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2023 ForgeFlow S.L.
 # Copyright 2017 Serpent Consulting Services Pvt. Ltd.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0).
 
 {
     "name": "Totals in analytic wip report view",
-    "version": "10.0.1.0.0",
-    "author": "Eficent",
+    "version": "14.0.1.0.0",
+    "author": "ForgeFlow",
     "category": "Projects",
     "summary": """Add the totals in tree view""",
     "website": "https://github.com/ForgeFlow/eficent-odoo-addons",
