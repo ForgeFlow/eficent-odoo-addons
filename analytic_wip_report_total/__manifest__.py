4d77c643726a80bd21d6ac79e5f92c2db0a9d3c8
JasminSForgeFlow <jasmin.solanki@forgeflow.com>
2023-03-17 16:06:58 +0530

    [IMP] analytic_wip_report_total: black, isort, prettier

--- analytic_wip_report_total/__manifest__.py
+++ analytic_wip_report_total/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # Copyright 2017 Serpent Consulting Services Pvt. Ltd.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0).
@@ -9,13 +8,13 @@
     "author": "Eficent",
     "category": "Projects",
     "summary": """Add the totals in tree view""",
-    "website": "http://www.eficent.com/",
+    "website": "https://github.com/ForgeFlow/eficent-odoo-addons",
     "license": "LGPL-3",
     "depends": [
         "analytic_wip_report_extend",
     ],
     "data": [
-        'views/wip_report_view.xml',
+        "views/wip_report_view.xml",
     ],
     "installable": True,
 }
