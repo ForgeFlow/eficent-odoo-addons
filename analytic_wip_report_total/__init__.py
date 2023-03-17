81ca34ac427b6e7b573c4bf751800fb1c246db73
JasminSForgeFlow <jasmin.solanki@forgeflow.com>
2023-03-17 16:41:27 +0530

    [MIG] analytic_wip_report_total: Migration to 14.0

--- analytic_wip_report_total/__init__.py
+++ analytic_wip_report_total/__init__.py
@@ -1,5 +1,3 @@
-# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2023 ForgeFlow S.L.
 # Copyright 2017 Serpent Consulting Services Pvt. Ltd.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0).
-
-from . import *
