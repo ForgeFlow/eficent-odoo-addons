b0030f89ac108964db07d1edcc19142841e6df2a
JasminSForgeFlow <jasmin.solanki@forgeflow.com>
2023-03-20 16:50:52 +0530

    [MIG] analytic_category: Migration to 14.0

--- analytic_category/__manifest__.py
+++ analytic_category/__manifest__.py
@@ -1,14 +1,13 @@
-# Copyright 2018 Eficent Business and IT Consulting Services S.L.
+# Copyright 2023 ForgeFlow S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Analytic Category",
-    "summary": """
- Allow to define analytic categories for analytic accounts
-
-    """,
-    "version": "10.0.1.0.0",
-    "author": "Eficent",
+    "summary": """Allow to define analytic categories for analytic accounts""",
+    "version": "14.0.1.0.0",
+    "author": "ForgeFlow",
+    "website": "https://github.com/ForgeFlow/eficent-odoo-addons",
     "category": "Analytic",
+    "license": "LGPL-3",
     "depends": ["analytic"],
     "data": [
         "security/security.xml",
