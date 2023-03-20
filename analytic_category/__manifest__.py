800be4c54364afa5a7254a529ea558b175b4cf90
JasminSForgeFlow <jasmin.solanki@forgeflow.com>
2023-03-20 14:50:01 +0530

    [IMP] analytic_category: black, isort, prettier

--- analytic_category/__manifest__.py
+++ analytic_category/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
@@ -8,11 +7,13 @@
 
     """,
     "version": "10.0.1.0.0",
-    "author":  "Eficent",
+    "author": "Eficent",
     "category": "Analytic",
     "depends": ["analytic"],
-    "data": ["security/security.xml",
-             "view/analytic_view.xml",
-             "view/category_view.xml"],
-    "installable": True
+    "data": [
+        "security/security.xml",
+        "view/analytic_view.xml",
+        "view/category_view.xml",
+    ],
+    "installable": True,
 }
