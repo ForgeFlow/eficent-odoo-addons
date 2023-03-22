005c35d676bddbe4592ed05aabe349f02896a1dc
JasminSForgeFlow <jasmin.solanki@forgeflow.com>
2023-03-22 11:28:47 +0530

    [MIG] analytic_plan_cost_revenue: Migration to 15.0

--- analytic_plan_cost_revenue/__manifest__.py
+++ analytic_plan_cost_revenue/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Analytic Plan Cost and Revenue",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "ForgeFlow",
     "category": "Projects",
     "Summary": """
