663b8ca29138fd680bb53a5846c0e18175dbb492
JasminSForgeFlow <jasmin.solanki@forgeflow.com>
2023-03-22 16:40:51 +0530

    [MIG] analytic_wip_report_extend: Migration to 15.0

--- analytic_wip_report_extend/__manifest__.py
+++ analytic_wip_report_extend/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "Work In Progress Statement Extend",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "ForgeFlow",
     "category": "Projects",
     "summary": """This adds new fields to analytic wip statement report""",
