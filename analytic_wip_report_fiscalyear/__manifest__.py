81e2c5e42f7d156b0917a7774d753d6c15494506
JasminSForgeFlow <jasmin.solanki@forgeflow.com>
2023-03-22 15:17:35 +0530

    [MIG] analytic_wip_report_fiscalyear: Migration to 15.0

--- analytic_wip_report_fiscalyear/__manifest__.py
+++ analytic_wip_report_fiscalyear/__manifest__.py
@@ -3,15 +3,12 @@
 
 {
     "name": "Work In Progress Statement Fiscal Year",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "ForgeFlow, Odoo Community Association (OCA), " "Project Expert Team",
     "website": "https://github.com/ForgeFlow/eficent-odoo-addons",
     "category": "Project Management",
     "license": "AGPL-3",
-    "depends": [
-        "analytic_wip_report",
-        "account_fiscal_year",
-    ],
+    "depends": ["analytic_wip_report", "account_fiscal_year", "date_range"],
     "data": [
         "views/account_analytic_account_view.xml",
         "wizards/analytic_wip_report_view.xml",
