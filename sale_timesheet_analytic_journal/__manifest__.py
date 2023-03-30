64c6e8c2f0af5978e821d4bf77bc7d01aa081057
Juany Davila <juanyadavilap@gmail.com>
2023-03-30 12:59:06 -0500

    [IMP] sale_timesheet_analytic_journal: black, isort, prettier

--- sale_timesheet_analytic_journal/__manifest__.py
+++ sale_timesheet_analytic_journal/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2014-17 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
@@ -8,15 +7,16 @@
     "author": "Eficent",
     "category": "HR",
     "summary": """Analytic journal for timesheets""",
-    "website": "http://www.eficent.com/",
+    "website": "https://github.com/ForgeFlow/eficent-odoo-addons",
     "license": "AGPL-3",
     "depends": [
-        'sale_timesheet',
-        'analytic_cost_revenue',
-        'analytic_journal',
+        "sale_timesheet",
+        "analytic_cost_revenue",
+        "analytic_journal",
     ],
-    "data": ['demo/analytic_journal_data.xml',
-             ],
-    'installable': True,
-    'auto_install': True,
+    "data": [
+        "demo/analytic_journal_data.xml",
+    ],
+    "installable": True,
+    "auto_install": True,
 }
